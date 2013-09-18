78f214e3c9f2cc840d3d9960528418ae4ab6988c
jamesl1001 <jamesl1001@msn.com>
2013-09-18 20:02:20 +0100

    Setup Grunt tasks

---
+++ Gruntfile.js
@@ -0,0 +1,82 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        autoprefixer: {
+            single: {
+                flatten: true,
+                src: 'css/styles.css',
+                dest: 'css/styles.css'
+            }
+        },
+        cssmin: {
+            minify: {
+                src: 'css/styles.css',
+                dest: 'build/styles.min.css'
+            }
+        },
+        jshint: {
+            all: ['js/*.js'],
+            options: {
+                camelcase: true,
+                curly: true,
+                indent: 4,
+                noempty: true,
+                quotmark: 'single',
+                undef: true,
+                unused: true,
+                trailing: true,
+                multistr: true
+            }
+        },
+        sass: {
+            dist: {
+                options: {
+                    style: 'compressed'
+                },
+                files: {
+                    'css/styles.css': 'sass/styles.scss'
+                }
+            },
+            dev: {
+                options: {
+                    style: 'expanded'
+                },
+                files: {
+                    'css/styles.css': 'sass/styles.scss'
+                }
+            }
+        },
+        uglify: {
+            build: {
+                src: 'js/*.js',
+                dest: 'build/scripts.min.js'
+            }
+        },
+        watch: {
+            scripts: {
+                files: [
+                    'sass/styles.scss'
+                ],
+                tasks: ['sass:dev', 'autoprefixer']
+            }
+        }
+    });
+
+    // Load tasks
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-autoprefixer');
+
+    // Register tasks
+    grunt.registerTask('default', 'List grunt tasks', function() {
+        grunt.log.writeln('\n  grunt sass:watch : sass:watch, autoprefix\
+                           \n  grunt jshint     : jshint\
+                           \n  grunt build      : cssmin, uglify');
+    });
+
+    grunt.registerTask('sass:watch', ['watch']);
+    grunt.registerTask('build', ['cssmin', 'uglify']);
+};
