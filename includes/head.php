22ad00c581c616e06367258a88b32b4ad137fc92
jamesl1001 <jamesl1001@msn.com>
2013-09-21 01:04:03 +0100

    Load minified CSS and JS on live env, unminified on sandbox env

--- includes/head.php
+++ includes/head.php
@@ -3,5 +3,9 @@
 <meta name="author" content="Precise Pixels"/>
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"/>
 <title>Cell</title>
-<link rel="stylesheet" type="text/css" href="/css/styles.css" />
+<?php if($_SERVER['SERVER_NAME'] == 'cell.dev'): ?>
+    <link rel="stylesheet" type="text/css" href="/css/styles.css"/>
+<?php else: ?>
+    <link rel="stylesheet" type="text/css" href="/build/styles.min.css"/>
+<?php endif; ?>
 <link rel="shortcut icon" href="/img/favicon.ico"/>
--- includes/scripts.php
+++ includes/scripts.php
@@ -1,1 +1,7 @@
 <script src='https://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js'></script>
+
+<?php if($_SERVER['SERVER_NAME'] == 'cell.dev'): ?>
+    <!-- individual scripts <script src='/js/*.js'></script> -->
+<?php else: ?>
+    <script src='/build/scripts.min.js'></script>
+<?php endif; ?>
