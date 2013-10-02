9685b0224c86553f73308f1684ac8e7e932f5711
Jacob Hammond <ss_clan_cod@hotmail.com>
2013-10-02 01:01:36 +0100

    Created spinning logo and added background to page

--- views/index.php
+++ views/index.php
@@ -1,1 +1,19 @@
-<h1>INDEX</h1>
+<nav></nav>
+
+<div id="wrapper">
+
+    <div id="splash-page">
+        
+    <img src="images/logo-cell.png" class="logo"/>
+    <div class="cube">
+        <div class="front dark">FRONT</div>
+        <div class="back dark">BACK</div>
+        <div class="left light">LEFT</div>
+        <div class="right dark">RIGHT</div>
+    </div>
+
+
+    </div>
+
+</div>
+
