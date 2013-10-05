55e270f3428498c1b559620f1d580ec7909dd168
jamesl1001 <jamesl1001@msn.com>
2013-10-05 17:06:26 +0100

    Add correct links to nav

--- includes/nav.php
+++ includes/nav.php
@@ -1,8 +1,8 @@
 <nav id="nav">&#9776;
     <div id="menu">
-        <a href="#home"><span class="menu__icon">&#9733;</span><span class="menu__label">Home</span></a>
-        <a href="#about"><span class="menu__icon">&#9728;</span><span class="menu__label">About</span></a>
-        <a href="#mycell"><span class="menu__icon">&#10084;</span><span class="menu__label">MyCell</span></a>
-        <a href="#contact"><span class="menu__icon">&#9762;</span><span class="menu__label">Contact</span></a>
+        <a href="index"><span class="menu__icon">&#9733;</span><span class="menu__label">Home</span></a>
+        <a href="about"><span class="menu__icon">&#9728;</span><span class="menu__label">About</span></a>
+        <a href="mycell"><span class="menu__icon">&#10084;</span><span class="menu__label">MyCell</span></a>
+        <a href="contact"><span class="menu__icon">&#9762;</span><span class="menu__label">Contact</span></a>
     </div>
 </nav>
