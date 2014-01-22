365f4e8f772d259a9bfa172fe03675d958fc3450
jamesl1001 <jamesl1001@msn.com>
2014-01-22 17:07:45 +0000

    Initial page setup

--- views/user/james/env/1.php
+++ views/user/james/env/1.php
@@ -1,1 +1,9 @@
 <h1>JAMES ENV 1</h1>
+
+<div id="env">ENV</div>
+
+<div id="env-info">ENV INFO</div>
+
+<div id="env-views">ENV VIEWS</div>
+
+<div id="more-envs">MORE-ENVS (this should be post-loaded on desktop only)</div>
