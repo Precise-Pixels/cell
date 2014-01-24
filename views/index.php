3ac5213897e36bdeaab30e814493298ad573e99a
Jacob Hammond <ss_clan_cod@hotmail.com>
2014-01-24 14:07:13 +0000

    Fixed code layout

--- views/index.php
+++ views/index.php
@@ -4,16 +4,17 @@
 
     <div id="splash-page">
         
-    <img src="images/logo-cell.png" class="logo"/>
-    <div class="cube">
-        <div class="front dark">FRONT</div>
-        <div class="back dark">BACK</div>
-        <div class="left light">LEFT</div>
-        <div class="right dark">RIGHT</div>
+        <img src="images/logo-cell.png" class="logo"/>
+        <div class="cube">
+            <div class="front dark">FRONT</div>
+            <div class="back dark">BACK</div>
+            <div class="left light">LEFT</div>
+            <div class="right dark">RIGHT</div>
+        </div>
+
     </div>
 
-
-    </div>
+    
 
 </div>
 
