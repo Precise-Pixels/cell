ce91cdfe8dce223f75165b5cd55f2f2abce5c633
faooful <faooful@gmail.com>
2014-01-26 20:13:34 +0000

    Implemented the header.

---
+++ includes/header.php
@@ -0,0 +1,8 @@
+<header>
+    <div class="nav-btn"></div>
+    <div class="user">
+        <div class="userpic"></div>
+        <div class="message">Welcome back, James</div>
+        <div class="usermenu"></div>
+    </div>
+</header>
