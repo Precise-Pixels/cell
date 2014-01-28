326b3bf2ed393dd7161c180a6b663ce779e6069c
Jacob Hammond <ss_clan_cod@hotmail.com>
2014-01-28 00:38:43 +0000

    Added Views menu button and menu using checkbox hack

--- views/user/james/env/1.php
+++ views/user/james/env/1.php
@@ -23,18 +23,20 @@
 
     <div id="model"></div>
     
+    <div id="model-interaction">
+        <p>Interact</p>
+        <div id="default" class="btn btn--interact btn--selected"><img src="/img/icons/ico_touch.png" alt="Use Touch"></div>
+        <div id="webcam" class="btn btn--interact"><img src="/img/icons/ico_webcam.png" alt="Use Webcam"></div>
+    </div>
     <div id="model-controls">
-        <div id="model-interaction">
-            <h2>Interact</h2>
-            <div id="default" class="btn btn--interact btn--selected"><img src="/img/icons/ico_touch.png" alt="Use Touch"></div>
-            <div id="webcam" class="btn btn--interact"><img src="/img/icons/ico_webcam.png" alt="Use Webcam"></div>
-        </div>
+            <input type="checkbox" id="menu-toggle"/>
+            <label for="menu-toggle" id="menu-btn" class="btn--menu"></label>
         <div id="model-views">
-            <h2>Views</h2>
+            <p>Views</p>
             <div id="360" class="btn btn--views btn--selected">360</div>
             <div id="top" class="btn btn--views">TOP</div>
             <div id="side" class="btn btn--views">SIDE</div>
-            <div id="live" class="btn btn--views">LIVE</div>
+            <div id="live" class="btn btn--views btn--views--live">LIVE</div>
         </div>      
     </div>
 
