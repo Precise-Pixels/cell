8ae8bbe50c2d7ce303adee7d0898e5363afd4511
Jacob Hammond <ss_clan_cod@hotmail.com>
2014-01-29 02:25:26 +0000

    Added demo environment files

--- js/env/env.js
+++ js/env/env.js
@@ -93,7 +93,7 @@ if(Detector.webgl) {
             scene.add( object );
 
         });
-        loader.load( '/3d/models/city.obj', '/3d/materials/city.mtl' );
+        loader.load( '/3d/models/mountain.obj', '/3d/materials/mountain.mtl' );
 
         // render
         renderer = new THREE.WebGLRenderer( { alpha: true, antialias: true } );
