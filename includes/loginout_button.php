77f11415f53611d03cd88c6b6edaa7cae1bfe32e
jamesl1001 <jamesl1001@msn.com>
2014-02-07 16:09:16 +0000

    Create markup and basic styling for header navs using checkbox hacks

--- includes/loginout_button.php
+++
@@ -1,6 +0,0 @@
-<?php
-if(isset($_SESSION['status']) && $_SESSION['status'] == 'loggedin') {
-    echo '<a href="logout" class="btn">LOGOUT</a>';
-} else {
-    echo '<a href="login" class="btn">LOGIN / REGISTER</a>';
-}
