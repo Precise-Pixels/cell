840ac0323a3b1e85ed2b184c3785e39216c0cd67
jamesl1001 <jamesl1001@msn.com>
2014-02-07 15:00:55 +0000

    Add 'REGISTER' to login button

--- includes/loginout_button.php
+++ includes/loginout_button.php
@@ -2,5 +2,5 @@
 if(isset($_SESSION['status']) && $_SESSION['status'] == 'loggedin') {
     echo '<a href="logout" class="btn">LOGOUT</a>';
 } else {
-    echo '<a href="login" class="btn">LOGIN</a>';
+    echo '<a href="login" class="btn">LOGIN / REGISTER</a>';
 }
