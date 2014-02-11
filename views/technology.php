89b10eed350b93a0cf38888b082fcc344656ee15
jamesl1001 <jamesl1001@msn.com>
2014-02-11 20:59:54 +0000

    Block out Technology page

--- views/technology.php
+++ views/technology.php
@@ -3,8 +3,45 @@
     <header class="section-padding align-centre dgrey">
         <hgroup class="align-vertical">
             <h1>TECHNOLOGY</h1>
-            <h2>IN DEPTH: CLONE CUBE</h2>
+            <h2>IN DEPTH: QuantumCell&trade; CLONE CUBE</h2>
         </hgroup>
     </header>
+
+    <section>
+        <div class="section-padding align-centre dgrey">
+            <img src="/img/placeholder.gif" alt="Clone Cube" class="full">
+        </div>
+    </section>
+
+    <section>
+        <div class="section-padding align-centre lgrey">
+            <h1>SPECIFICATIONS</h1>
+
+            <h2 class="third-margin">Physical</h2>
+            <h2 class="third-margin">Power</h2>
+            <h2 class="third-margin">Sensors</h2>
+
+            <p class="third-margin">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent massa libero, rhoncus at sodales et, fringilla vitae justo. Suspendisse adipiscing nunc tortor, eget accumsan tellus tincidunt at. Ut suscipit elit id tellus tincidunt, ac luctus nisi consectetur. Proin velit libero, faucibus sed ultricies eget, pulvinar sit amet erat. Suspendisse vel leo sit amet magna sodales tincidunt.</p>
+            <p class="third-margin">Integer sit amet nibh sit amet lectus luctus placerat. Suspendisse eget arcu tortor. Fusce pretium sodales nisl et tempus. Curabitur pretium pellentesque libero. Curabitur pretium malesuada purus, id posuere neque. Donec ante metus, rutrum id lacus ut, ultricies aliquam magna.</p>
+            <p class="third-margin">Donec consequat est sed eros condimentum, nec porttitor mi convallis. Sed porttitor, justo a blandit interdum, lorem nulla ullamcorper eros, sit amet convallis ipsum ligula eu lorem. Praesent elementum diam at mauris sodales, et pellentesque nibh dignissim.</p>
+        </div>
+    </section>
+
+    <section>
+        <div class="align-centre">
+            <div class="cta cta--example half">
+                <hgroup class="align-vertical">
+                    <h1>CTA Title</h1>
+                    <h2>Optional subtitle</h2>
+                </hgroup>
+            </div>
+            <div class="cta cta--example half">
+                <hgroup class="align-vertical">
+                    <h1>CTA Title</h1>
+                    <h2>Optional subtitle</h2>
+                </hgroup>
+            </div>
+        </div>
+    </section>
 
 </main>
