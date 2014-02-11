7803f92eaeb8fc224f62fa52d5eaa8ff17a81442
faooful <faooful@gmail.com>
2014-02-11 20:21:09 +0000

    initial blocks to the about page.

--- views/about.php
+++ views/about.php
@@ -6,4 +6,66 @@
         </hgroup>
     </header>
 
+    <section>
+        <div class="section-padding align-centre lgrey">
+            <h1>COMPANY</h1>
+            <div class="section-content-wrapper">
+                <h2>.half-margin</h2>
+                <p class="half-margin">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent massa libero, rhoncus at sodales et, fringilla vitae justo. Suspendisse adipiscing nunc tortor, eget accumsan tellus tincidunt at. Ut suscipit elit id tellus tincidunt, ac luctus nisi consectetur. Proin velit libero, faucibus sed ultricies eget, pulvinar sit amet erat. Suspendisse vel leo sit amet magna sodales tincidunt.</p>
+                <p class="half-margin">Integer sit amet nibh sit amet lectus luctus placerat. Suspendisse eget arcu tortor. Fusce pretium sodales nisl et tempus. Curabitur pretium pellentesque libero. Curabitur pretium malesuada purus, id posuere neque. Donec ante metus, rutrum id lacus ut, ultricies aliquam magna.</p>
+            </div>
+        </div>
+    </section>
+
+    <section>
+        <div class="section-padding align-centre lgrey">
+            <h1>KEY PEOPLE</h1>
+
+            <div class="hoverbox third-margin">
+                <figure><img src="/img/placeholder.gif" alt="Hoverbox image"/></figure>
+                <figcaption>
+                    <h1>DR REE</h1>
+                    <p>This portion of the caption is hidden until the image is hovered.</p>
+                </figcaption>
+            </div>
+
+            <div class="hoverbox third-margin">
+                <figure><img src="/img/placeholder.gif" alt="Hoverbox image"/></figure>
+                <figcaption>
+                    <h1>JAKOB HAMMON</h1>
+                    <p>This portion of the caption is hidden until the image is hovered.</p>
+                </figcaption>
+            </div>
+
+            <div class="hoverbox third-margin">
+                <figure><img src="/img/placeholder.gif" alt="Hoverbox image"/></figure>
+                <figcaption>
+                    <h1>JASOP WILHIELM</h1>
+                    <p>This portion of the caption is hidden until the image is hovered.</p>
+                </figcaption>
+            </div>
+        </div>
+    </section>
+
+    <section>
+        <div class="section-padding align-centre lgrey">
+            <h1>TIMELINE</h1>
+        </div>
+    </section>
+
+    <section class="section--spacer">
+        <div class="section-padding align-centre lgrey">
+            <div class="cta cta--example half">
+                <hgroup class="align-vertical">
+                    <h1>INFOGRAPHIC</h1>
+                </hgroup>
+            </div>
+            <div class="cta cta--example half">
+                <hgroup class="align-vertical">
+                    <h1>TECHNOLOGY</h1>
+                </hgroup>
+            </div>
+        </div>
+    </section>
+
 </main>
