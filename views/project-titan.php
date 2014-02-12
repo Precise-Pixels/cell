c4ed1508e11c147e14ac99f3bf5cd45406d63767
faooful <faooful@gmail.com>
2014-02-12 12:26:11 +0000

    made the image full width on the project-titan page.

--- views/project-titan.php
+++ views/project-titan.php
@@ -20,18 +20,24 @@
     </section>
 
     <section>
-        <div class="section-padding full">
+        <div class="align-centre">
             <img src="/img/placeholder.gif" alt="Clone Cube" class="full"/>
         </div>
     </section>
 
     <section>
         <div class="section-padding align-centre sdgrey">
-            <h1>THE COMPANY</h1>
+            <h1>HOW YOU ARE INVOLVED</h1>
             <div class="section-content-wrapper">
                 <p class="half-margin">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent massa libero, rhoncus at sodales et, fringilla vitae justo. Suspendisse adipiscing nunc tortor, eget accumsan tellus tincidunt at. Ut suscipit elit id tellus tincidunt, ac luctus nisi consectetur. Proin velit libero, faucibus sed ultricies eget, pulvinar sit amet erat. Suspendisse vel leo sit amet magna sodales tincidunt.</p>
                 <p class="half-margin">Integer sit amet nibh sit amet lectus luctus placerat. Suspendisse eget arcu tortor. Fusce pretium sodales nisl et tempus. Curabitur pretium pellentesque libero. Curabitur pretium malesuada purus, id posuere neque. Donec ante metus, rutrum id lacus ut, ultricies aliquam magna.</p>
             </div>
+        </div>
+    </section>
+
+    <section>
+        <div class="align-centre">
+            <img src="/img/placeholder.gif" alt="Clone Cube" class="full"/>
         </div>
     </section>
 
