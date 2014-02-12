48b8316c85b35dbc1e9591fb8b0e88f2dc4e9de9
faooful <faooful@gmail.com>
2014-02-12 13:53:18 +0000

    added in a style to the typography page for a centered text class. begun to block out the index page.

--- views/index.php
+++ views/index.php
@@ -6,4 +6,26 @@
         </hgroup>
     </header>
 
+    <header class="section-padding align-centre mblue">
+        <hgroup class="align-vertical">
+            <h1>PROJECT TITAN</h1>
+            <h2>SUBHEADER</h2>
+        </hgroup>
+    </header>
+
+    <section>
+        <div class="align-centre">
+            <img src="/img/placeholder.gif" alt="Clone Cube" class="full"/>
+        </div>
+    </section>
+
+    <section class="mblue">
+        <div class="section-padding align-centre">
+            <h1>FULL BLEED SECTION</h1>
+                <p class="third-margin">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent massa libero, rhoncus at sodales et, fringilla vitae justo. Suspendisse adipiscing nunc tortor, eget accumsan tellus tincidunt at. Ut suscipit elit id tellus tincidunt, ac luctus nisi consectetur. Proin velit libero, faucibus sed ultricies eget, pulvinar sit amet erat. Suspendisse vel leo sit amet magna sodales tincidunt.</p>
+                <a href="#" class="btn third-margin">LEARN MORE</a>
+                <p class="third-margin">Donec consequat est sed eros condimentum, nec porttitor mi convallis. Sed porttitor, justo a blandit interdum, lorem nulla ullamcorper eros, sit amet convallis ipsum ligula eu lorem. Praesent elementum diam at mauris sodales, et pellentesque nibh dignissim.</p>
+        </div>
+    </section>
+
 </main>
