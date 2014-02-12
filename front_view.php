edb0854f9dc6c395974603d35175d1722d3adaec
jamesl1001 <jamesl1001@msn.com>
2014-02-12 16:59:44 +0000

    Remove bodyclass.php for the timebeing

--- front_view.php
+++ front_view.php
@@ -5,7 +5,7 @@
 <head>
     <?php require_once('includes/head.php'); ?>
 </head>
-<body class="<?php require_once('includes/bodyclass.php'); ?>">
+<body>
     <?php require_once('includes/header.php'); ?>
     <?php require_once("views/$file.php"); ?>
     <?php require_once('includes/footer.php'); ?>
--- includes/bodyclass.php
+++
@@ -1,2 +0,0 @@
-<?php
-echo str_replace('/', '_', substr(parse_url($_SERVER['REQUEST_URI'])['path'], 1));
