2aeeab348d782508d2fd366566f20a79feaff83e
faooful <faooful@gmail.com>
2014-02-12 11:43:49 +0000

    added the split color section.

--- views/project-titan.php
+++ views/project-titan.php
@@ -6,4 +6,17 @@
         </hgroup>
     </header>
 
+    <section>
+        <div class="align-centre lgrey">
+            <div class="section-padding half">
+                <h1>REVIEWS</h1>
+                <p class="full-margin">Integer sit amet nibh sit amet lectus luctus placerat. Suspendisse eget arcu tortor. Fusce pretium sodales nisl et tempus.</p>
+            </div>
+            <div class="section-padding half mblue">
+                <h1>QUOTE</h1>
+                <p class="full-margin">Integer sit amet nibh sit amet lectus luctus placerat. Suspendisse eget arcu tortor. Fusce pretium sodales nisl et tempus.</p>
+            </div>
+        </div>
+    </section>
+
 </main>
