9e95527bf265d2e70842e22e2bc886e56f54507b
jamesl1001 <jamesl1001@msn.com>
2014-02-12 14:06:41 +0000

    Add TED Talk page

---
+++ views/ted.php
@@ -0,0 +1,22 @@
+<main>
+
+    <header class="section-padding align-centre dgrey">
+        <hgroup class="align-vertical">
+            <h1>TED TALK</h1>
+        </hgroup>
+    </header>
+
+    <section>
+        <div class="align-centre">
+            <img src="/img/placeholder.gif" alt="TED Talk" class="full"/>
+        </div>
+    </section>
+
+    <section>
+        <div class="section-padding align-centre lgrey">
+            <h1>TRANSCRIPT</h1>
+            <p>Integer sit amet nibh sit amet lectus luctus placerat. Suspendisse eget arcu tortor. Fusce pretium sodales nisl et tempus.</p>
+        </div>
+    </section>
+
+</main>
