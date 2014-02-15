30fa3a8eddded395578f78c71019845aeae90a61
jamesl1001 <jamesl1001@msn.com>
2014-02-15 12:59:33 +0000

    Remove directory index logic from front-controller

--- front-controller.php
+++ front-controller.php
@@ -4,12 +4,8 @@ $q = $_GET['q'];
 
 $path = preg_replace('/\/$|.php/', '', $q);
 
-// NB: might be able to remove this after env/user views are done
-
 if(empty($path)) {                                  // HOME
     $file = 'index';
-} elseif(file_exists("views/$path/index.php")) {    // DIRECTORY INDEX
-    $file = "$path/index";
 } elseif(file_exists("views/$path.php")) {          // PAGE
     $file = $path;
 } else {                                            // NOT FOUND
