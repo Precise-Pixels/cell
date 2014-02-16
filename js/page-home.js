d465a626bd470558953adf41ef924daa564485bc
faooful <faooful@gmail.com>
2014-02-16 23:53:35 +0000

    added a resize function.

--- js/page-home.js
+++ js/page-home.js
@@ -1,1 +1,6 @@
-document.getElementById('primary-header').style.height = (window.innerHeight || document.documentElement.clientHeight) - 40 + 'px'; 
+var primaryHeader = document.getElementById('primary-header');
+primaryHeader.style.height =  (window.innerHeight || document.documentElement.clientHeight) - 40 + 'px';
+
+window.onresize = function() {
+    primaryHeader.style.height =  (window.innerHeight || document.documentElement.clientHeight) - 40 + 'px';
+}; 
