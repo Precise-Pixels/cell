241db1408f4639ecd188c01622b1a4b9ecce4a43
faooful <faooful@gmail.com>
2014-02-17 11:45:54 +0000

    added the social icon containers to the framework.

--- views/_template.php
+++ views/_template.php
@@ -120,4 +120,35 @@
         </div>
     </section>
 
+    <section>
+        <div class="section-padding align-centre dgrey">
+            <h1>SOCIAL BUTTONS</h1>
+            <div class="social facebook">
+                <a href="#">
+                    <div class="overlay"></div>
+                </a>
+            </div>
+            <div class="social twitter">
+                <a href="#">
+                    <div class="overlay"></div>
+                </a>
+            </div>
+            <div class="social google-plus">
+                <a href="#">
+                    <div class="overlay"></div>
+                </a>
+            </div>
+            <div class="social mail">
+                <a href="#">
+                    <div class="overlay"></div>
+                </a>
+            </div>
+            <div class="social linkedin">
+                <a href="#">
+                    <div class="overlay"></div>
+                </a>
+            </div>
+        </div>
+    </section>
+
 </main>
