767aea26dcc231827c2f9d0dfafed87b988844f4
faooful <faooful@gmail.com>
2014-02-17 16:20:56 +0000

    used the button styles from the style sheet.

--- includes/footer.php
+++ includes/footer.php
@@ -1,33 +1,28 @@
-<footer class="align-centre dgrey">
-        <section>
-        <div class="section-padding align-centre dgrey">
-            <h1>SOCIAL BUTTONS</h1>
-            <div class="social facebook">
-                <a href="#">
-                    <div class="icon facebook-icon"></div>
-                    <div class="overlay"></div>
-                </a>
-            </div>
-            <div class="social twitter">
-                <a href="#">
-                    <div class="overlay"></div>
-                </a>
-            </div>
-            <div class="social google-plus">
-                <a href="#">
-                    <div class="overlay"></div>
-                </a>
-            </div>
-            <div class="social mail">
-                <a href="#">
-                    <div class="overlay"></div>
-                </a>
-            </div>
-            <div class="social linkedin">
-                <a href="#">
-                    <div class="overlay"></div>
-                </a>
-            </div>
-        </div>
-    </section>
+<footer class="section-padding align-centre dgrey">
+    <h1>SOCIAL BUTTONS</h1>
+    <div id="btn--facebook" class="btn">
+        <a href="#">
+            <div class="icon icon--facebook"></div>
+        </a>
+    </div>
+    <div id="btn--twitter" class="btn">
+        <a href="#">
+            <div class="icon icon--twitter"></div>
+        </a>
+    </div>
+    <div id="btn--google-plus" class="btn">
+        <a href="#">
+            <div class="icon icon--google-plus"></div>
+        </a>
+    </div>
+    <div id="btn--mail" class="btn">
+        <a href="#">
+            <div class="icon icon--mail"></div>
+        </a>
+    </div>
+    <div id="btn--linkedin" class="btn">
+        <a href="#">
+            <div class="icon icon--linkedin"></div>
+        </a>
+    </div>
 </footer>
