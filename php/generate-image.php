85e6e652eb6cf1cb9fdd73ebc636dc685bdcf338
jamesl1001 <jamesl1001@msn.com>
2014-02-17 21:45:45 +0000

    Blur generated height map

--- php/generate-image.php
+++ php/generate-image.php
@@ -9,16 +9,11 @@ $imgWidth = $imgHeight = $resolution;
 $img = imagecreatetruecolor($imgWidth, $imgHeight);
 
 $rows = explode('-', $heightsString);
-// var_dump($rows);
 
 foreach($rows as $y => $row) {
     $columns = explode(',', $row);
-    // var_dump($columns);
     foreach($columns as $x => $column) {
-        // var_dump($column);
         $colour = imagecolorallocate($img, $column, $column, $column);
-        // var_dump($x,$y);
-        // var_dump($colour);
         imagesetpixel($img,$x,$y,$colour);
     }
 }
@@ -26,16 +21,9 @@ foreach($rows as $y => $row) {
 $scaledImg = imagecreatetruecolor($tileSize, $tileSize);
 imagecopyresampled($scaledImg, $img, 0, 0, 0, 0, $tileSize, $tileSize, $resolution, $resolution);
 
-// imagefilter($scaledImg, IMG_FILTER_GAUSSIAN_BLUR);
-// imagefilter($scaledImg, IMG_FILTER_SMOOTH, -2);
-// imagefilter($scaledImg, IMG_FILTER_GAUSSIAN_BLUR);
-
-// for ($i = 1; $i < 10; $i++) {
-    // imagefilter($scaledImg, IMG_FILTER_GAUSSIAN_BLUR);
-// }
+for ($i = 1; $i < 10; $i++) {
+    imagefilter($scaledImg, IMG_FILTER_GAUSSIAN_BLUR);
+}
 
 imagepng($scaledImg, 'generated-image.png');
 imagedestroy($scaledImg);
-
-// imagepng($img, 'generated-image.png');
-// imagedestroy($img);
