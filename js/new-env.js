cc302ed19121aae82f0650be819a30373ff1d707
jamesl1001 <jamesl1001@msn.com>
2014-02-18 15:44:45 +0000

    Change tileSize to better match Static Maps API maximum tile size

--- js/new-env.js
+++ js/new-env.js
@@ -8,7 +8,7 @@ var lon2;
 var centreLat;
 var centreLon;
 var resolution   = 30;
-var tileSize     = 256;
+var tileSize     = 320;
 var requiredZoom = 10;
 
 function init() {
@@ -62,7 +62,7 @@ function init() {
             // console.log([lat1, lon1, lat2, lon2, centreLat, centreLon]);
 
             var selectedTile = document.getElementById('selected-tile');
-            selectedTile.src = 'http://maps.googleapis.com/maps/api/staticmap?center=' + centreLat + ',' + centreLon + '&zoom=' + requiredZoom + '&size=' + tileSize + 'x' + tileSize + '&scale=1&maptype=satellite&sensor=false';
+            selectedTile.src = 'http://maps.googleapis.com/maps/api/staticmap?center=' + centreLat + ',' + centreLon + '&zoom=' + (requiredZoom + 1) + '&size=' + tileSize * 2 + 'x' + tileSize * 2 + '&scale=1&maptype=satellite&sensor=false';
             // Clear and load map grid overlay
             map.overlayMapTypes.setAt(0, null);
             map.overlayMapTypes.insertAt(0, tilelayer);
--- php/getEnvTexture.php
+++ php/getEnvTexture.php
@@ -3,7 +3,7 @@
 $lat = $_GET['lat'];
 $lon = $_GET['lon'];
 
-$img = imagecreatefrompng("http://maps.googleapis.com/maps/api/staticmap?center=$lat,$lon&zoom=11&size=512x512&scale=4&maptype=satellite&sensor=false");
+$img = imagecreatefrompng("http://maps.googleapis.com/maps/api/staticmap?center=$lat,$lon&zoom=11&size=640x640&scale=2&maptype=satellite&sensor=false");
 
 header('Content-Type: image/png');
 imagepng($img);
