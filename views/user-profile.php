a3088b65821d693ae7c6841a8adb5692d43237c9
Jacob Hammond <ss_clan_cod@hotmail.com>
2014-02-20 19:15:23 +0000

    Moved new-env button to profile banner

--- views/user-profile.php
+++ views/user-profile.php
@@ -12,12 +12,23 @@
                     <h2 class="align-vertical">No. Envs Cloned</h2>
                 </div>
                 <div class="half-margin">
+                    <?php if(isset($_SESSION['userId']) && $_SESSION['userId'] == $user->userId): // If user is signed in and viewing their own profile ?>  
+                     
+                    <a href="/user/<?= $username; ?>/env/new" alt="Clone a new environment"> 
+                        <div class="environment-block environment-block--new quarter-margin">
+                            <div id="new-env-icon"></div>
+                        </div>
+                    </a>
+                    
+                    <?php endif; ?>
                     <h2 class="align-vertical">Some other fact here</h2>
                 </div>
             </div>
         </div>
     </section>
     <!-- USER PROFILE BANNER END -->
+
+        
 
     <section>
         <div class="align-centre dgrey">
@@ -36,20 +47,10 @@
                 </div>
 
         <?php endforeach;
-        
         else: ?>
             <p>This user hasn't cloned any environments yet.</p>
         <?php endif; ?>
-        
-        <?php if(isset($_SESSION['userId']) && $_SESSION['userId'] == $user->userId): // If user is signed in and viewing their own profile ?>  
-         
-        <a href="/user/<?= $username; ?>/env/new" alt="Clone a new environment"> 
-            <div class="environment-block environment-block--new quarter-margin">
-                <div id="new-env-icon" class="vertical-align"></div>
-            </div>
-        </a>
-        
-        <?php endif; ?>
+
         
         </div>
     </section>
