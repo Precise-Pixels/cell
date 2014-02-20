a34f5a1576a6eecfa69fe453114e40174813148e
Jacob Hammond <ss_clan_cod@hotmail.com>
2014-02-20 03:05:04 +0000

    Altered quarter-margin styles

--- views/user-profile.php
+++ views/user-profile.php
@@ -9,20 +9,24 @@
                 <img id="user-profile-pic" src="http://www.gravatar.com/avatar/6c4d9d7f7412a5c4e1ac47acd2d24112?d=mm&s=350">
                 <h1><?= $user->username; ?></h1>
             </div>
-            <div class="three-quarters sdgrey">
-                <p>2 thirds</p>
+            <div class="three-quarters section-padding">
+                <div class="half-margin sdgrey">
+                    <h2 class="align-vertical">No. Envs Cloned</h2>
+                </div>
+                <div class="half-margin">
+                    <h2 class="align-vertical">Some other fact here</h2>
+                </div>
             </div>
         </div>
     </section>
 
     <section>
-        <div class="align-centre section-padding lgrey">
-        <h1>CLONED ENVIRONMENTS</h1>
+        <div class="align-centre dgrey">
         <?php if(!empty($environments)):
             foreach($environments as $env): ?>
                 <div class="user-environment quarter-margin mblue">
-<!--                <img src="http://maps.googleapis.com/maps/api/staticmap?center=<?= $env->latitude; ?>,<?= $env->longitude; ?>&amp;zoom=11&amp;size=640x640&amp;scale=1&amp;maptype=satellite&amp;sensor=false&amp;key=AIzaSyCNlx7Q6EFJ2nlJfkAnMIsCm94fdSzaqf4">
--->                 <p><?= $env->timestamp; ?></p>
+                <img src="http://maps.googleapis.com/maps/api/staticmap?center=<?= $env->latitude; ?>,<?= $env->longitude; ?>&amp;zoom=11&amp;size=640x640&amp;scale=1&amp;maptype=satellite&amp;sensor=false&amp;key=AIzaSyCNlx7Q6EFJ2nlJfkAnMIsCm94fdSzaqf4">
+                 <p><?= $env->timestamp; ?></p>
 <!--                <p><?= $env->latitude; ?></p>
                     <p><?= $env->longitude; ?></p> -->
                 </div>
