39030222cbe2e8019e97109b1f71e2918136e7e1
Jacob Hammond <ss_clan_cod@hotmail.com>
2014-02-20 18:18:28 +0000

    Changed styles for profile image and name

--- views/user-profile.php
+++ views/user-profile.php
@@ -1,15 +1,15 @@
 <main>
 
         <p><?= $user->userId; ?></p>
-        <p><?= $user->timestamp; ?></p>
 
+    <!-- USER PROFILE BANNER -->
     <section id="user-profile-banner" class="section--spacer">
-        <div  class="align-centre mblue">    
-            <div id="user-profile-card" class="quarter lgrey">
-                <img id="user-profile-pic" src="http://www.gravatar.com/avatar/<?= (isset($user->email) ? md5(strtolower(trim($user->email))) : 1); ?>?d=mm&amp;s=350" />
-                <h1><?= $user->username; ?></h1>
-<!--                 <h1><?= $user->email; ?></h1>
- -->            </div>
+        <div class="align-centre sdgrey">    
+            <div id="user-profile-card" class="quarter lgrey">                
+                <img src="http://www.gravatar.com/avatar/<?= (isset($user->email) ? md5(strtolower(trim($user->email))) : 1); ?>?d=mm&amp;s=350" />
+                <h1 class="lgrey"><?= $user->username; ?></h1>                
+            </div>
+
             <div class="three-quarters section-padding">
                 <div class="half-margin sdgrey">
                     <h2 class="align-vertical">No. Envs Cloned</h2>
@@ -20,29 +20,36 @@
             </div>
         </div>
     </section>
+    <!-- USER PROFILE BANNER END -->
 
     <section>
         <div class="align-centre dgrey">
         <?php if(!empty($environments)):
             foreach($environments as $env): ?>
+
             <a href="/user/<?= $user->username; ?>/env/<?= $env->envId; ?>">
-                <div class="user-environment quarter-margin mblue">
+                <div class="environment-block quarter-margin mblue">
                     <img src="/img/placeholder.gif">
-<!--                 <img src="http://maps.googleapis.com/maps/api/staticmap?center=<?= $env->latitude; ?>,<?= $env->longitude; ?>&amp;zoom=11&amp;size=640x640&amp;scale=1&amp;maptype=satellite&amp;sensor=false&amp;key=AIzaSyCNlx7Q6EFJ2nlJfkAnMIsCm94fdSzaqf4">
- -->                 <p><?= $env->timestamp; ?></p>
-                    <p><?= $env->name; ?></p>
-<!--                <p><?= $env->latitude; ?></p>
-                    <p><?= $env->longitude; ?></p> -->
+<!--               <img src="http://maps.googleapis.com/maps/api/staticmap?center=<?= $env->latitude; ?>,<?= $env->longitude; ?>&amp;zoom=11&amp;size=640x640&amp;scale=1&amp;maptype=satellite&amp;sensor=false&amp;key=AIzaSyCNlx7Q6EFJ2nlJfkAnMIsCm94fdSzaqf4">
+      -->          
+                    <div class="environment-block-details">
+                        <p><?= $env->name; ?></p>
+                    </div>
+
                 </div>
+
         <?php endforeach;
+        
         else: ?>
             <p>This user hasn't cloned any environments yet.</p>
         <?php endif; ?>
         
         <?php if(isset($_SESSION['userId']) && $_SESSION['userId'] == $user->userId): // If user is signed in and viewing their own profile ?>  
-            <div class="user-environment quarter-margin mblue">
+         
+            <div class="environment-block environment-block--new quarter-margin mblue">
                 <a href="/user/<?= $username; ?>/env/new">Clone a new environment</a>
             </div>
+        
         <?php endif; ?>
         
         </div>
