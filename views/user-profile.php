6bea7e9a70d0f5ce8360b9635121f7230d42ba6a
Jacob Hammond <ss_clan_cod@hotmail.com>
2014-02-21 18:19:22 +0000

    Changed profile card markup to iamgebox, removed spaces

--- views/user-profile.php
+++ views/user-profile.php
@@ -1,10 +1,14 @@
 <main>
     <!-- USER PROFILE BANNER -->
     <section id="user-profile-banner" class="sdgrey">
-        <div class="align-centre">    
-            <div id="user-profile-card" class="third-margin">                
-                <img src="http://www.gravatar.com/avatar/<?= (isset($user->email) ? md5(strtolower(trim($user->email))) : 1); ?>?d=mm&amp;s=350" />
-                <h1 class="mblue"><?= $user->username; ?></h1>                
+        <div class="align-centre">
+            <div class="imagebox third-margin">
+                <figure>
+                    <img src="http://www.gravatar.com/avatar/<?= (isset($user->email) ? md5(strtolower(trim($user->email))) : 1); ?>?d=mm&amp;s=350" />
+                </figure>
+                <figcaption>
+                    <h1><?= $user->username; ?></h1>
+                </figcaption>
             </div>
             <div class="third-margin">
                 <h2 class="">No. Envs Cloned</h2>
@@ -14,17 +18,17 @@
     </section>
     <!-- USER PROFILE BANNER END -->
 
-    <section id="environment-listing"> 
+    <section id="environment-listing">
         <div class="align-centre">
         <?php if(!empty($environments)):
             foreach($environments as $env): ?>
             <a href="/user/<?= $user->username; ?>/env/<?= $env->envId; ?>">
                 <div class="imagebox quarter mblue">
                     <figure>
-                        <img src="/img/placeholder.gif" alt="Imagebox image">
+                        <img src="/img/placeholder.gif" alt="<?= $env->name; ?>">
                     </figure>
                     <figcaption>
-                        <h2><?= $env->name; ?></h2>
+                        <h1><?= $env->name; ?></h1>
                     </figcaption>
                 </div>
             </a>
@@ -34,16 +38,16 @@
                 <h1>This user hasn't cloned any environments yet.</h1>
             </div>
         <?php endif; ?>
-        <!-- If user is signed in and viewing their own profile --> 
-        <?php if(isset($_SESSION['userId']) && $_SESSION['userId'] == $user->userId): ?>  
+        <!-- If user is signed in and viewing their own profile -->
+        <?php if(isset($_SESSION['userId']) && $_SESSION['userId'] == $user->userId): ?>
             <div class="quarter section-padding">
-                <a href="/user/<?= $username; ?>/env/new" alt="Clone a new environment"> 
+                <a href="/user/<?= $username; ?>/env/new" alt="Clone a new environment">
                     <div class="imagebox-new--environment">
                         <div id="new-env-icon"></div>
                     </div>
                 </a>
             </div>
-        <?php endif; ?>    
+        <?php endif; ?>
         </div>
     </section>
 </main>
