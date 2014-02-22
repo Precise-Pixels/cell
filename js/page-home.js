876f67f88808259ae0da9831c61632ed5ed732aa
faooful <faooful@gmail.com>
2014-02-22 10:42:46 +0000

    removed the double quotes.

--- js/page-home.js
+++ js/page-home.js
@@ -1,9 +1,9 @@
 var primaryHeader = document.getElementById('primary-header');
 var secondaryHeader = document.getElementById('secondary-header');
 primaryHeader.style.height = (window.innerHeight || document.documentElement.clientHeight) - 40 + 'px';
-secondaryHeader.style.marginTop = (parseInt(primaryHeader.style.height) + 40) + "px";
+secondaryHeader.style.marginTop = (parseInt(primaryHeader.style.height) + 40) + 'px';
 
 window.onresize = function() {
     primaryHeader.style.height = (window.innerHeight || document.documentElement.clientHeight) - 40 + 'px';
-    secondaryHeader.style.marginTop = (parseInt(primaryHeader.style.height) + 40) + "px";
+    secondaryHeader.style.marginTop = (parseInt(primaryHeader.style.height) + 40) + 'px';
 };
