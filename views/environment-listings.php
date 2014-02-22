3de3e8cd566d132f094544159503595a47922af7
Jacob Hammond <ss_clan_cod@hotmail.com>
2014-02-22 00:35:42 +0000

    Created environment-listings page

---
+++ views/environment-listings.php
@@ -0,0 +1,13 @@
+<main>
+
+    <header class="section-padding align-centre dgrey">
+        <hgroup class="align-vertical">
+            <h1>RECENTLY CLONED ENVIRONMENTS</h1>
+        </hgroup>
+    </header>
+
+    <section id="new-env-instructions" class="sdgrey section--spacer">
+        <div class="align-centre"></div>
+    </section>
+    
+</main>
