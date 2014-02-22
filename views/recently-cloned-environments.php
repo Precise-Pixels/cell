4c8a4f7a247e3fc50b9838221f9b706932b54afc
jamesl1001 <jamesl1001@msn.com>
2014-02-22 01:12:54 +0000

    Recently cloned environments backend database query

--- views/recently-cloned-environments.php
+++ views/recently-cloned-environments.php
@@ -11,24 +11,20 @@
     </section>
 
     <section id="environment-listing">
-    <div class="align-centre">
-        <?php if(!empty($environments)):
-            foreach($environments as $env): ?>
-                <a href="/user/<?= $user->username; ?>/env/<?= $env->envId; ?>">
-                    <div class="imagebox quarter mblue">
-                        <figure>
-                            <img src="/img/placeholder.gif" alt="<?= $env->name; ?>">
-                        </figure>
-                        <figcaption>
-                            <h1><?= $env->name; ?></h1>
-                        </figcaption>
-                    </div>
-                </a>
-        <?php endforeach;
-        else: ?>
-            <div id="no-environments" class="section-padding mblue">
-                <h1>This user hasn't cloned any environments yet.</h1>
-            </div>
-        <?php endif; ?>
+        <div class="align-centre">
+            <?php foreach($environments as $env): ?>
+                    <a href="/user/<?= $env->username; ?>/env/<?= $env->envId; ?>">
+                        <div class="imagebox quarter mblue">
+                            <figure>
+                                <img src="/img/placeholder.gif" alt="<?= $env->name; ?>">
+                            </figure>
+                            <figcaption>
+                                <h1><?= $env->name; ?></h1>
+                            </figcaption>
+                        </div>
+                    </a>
+            <?php endforeach; ?>
+        </div>
+    </section>
 
 </main>
