d4e455e33a83a52960ac61ee993833a6402dc10e
faooful <faooful@gmail.com>
2014-02-22 15:08:41 +0000

    fixed parallax on the cloning process view

--- views/the-cloning-process.php
+++ views/the-cloning-process.php
@@ -1,10 +1,10 @@
-<header id="parallax-header" class="section-padding align-centre dgrey">
+<header class="fixed-header section-padding align-centre dgrey">
     <hgroup class="align-vertical">
         <h1>THE CLONING PROCESS</h1>
     </hgroup>
 </header>
 
-<main class="parallax-margin">
+<main>
 
 
 </main>
