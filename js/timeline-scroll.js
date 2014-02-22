82dbdec4561c305a3922f95350d9fed029a2153c
faooful <faooful@gmail.com>
2014-02-22 11:44:33 +0000

    added 4 indents instead of 2

--- js/timeline-scroll.js
+++ js/timeline-scroll.js
@@ -5,21 +5,21 @@ var x = 0;
 var timeline = document.getElementById('timeline');
 
 timeline.addEventListener('mousedown', function(e) {
-  down = true;
-  scrollLeft = this.scrollLeft;
-  x = e.clientX; 
+    down = true;
+    scrollLeft = this.scrollLeft;
+    x = e.clientX; 
 });
 
 timeline.addEventListener('mouseup', function() {
-  down = false;
+    down = false;
 });
 
 timeline.addEventListener('mousemove', function(e) {
-  if (down) {
-    this.scrollLeft = scrollLeft + x - e.clientX;
-  }
+    if (down) {
+        this.scrollLeft = scrollLeft + x - e.clientX;
+    }
 });
 
 timeline.addEventListener('moveleave', function() {
-  down = false;
+    down = false;
 });
