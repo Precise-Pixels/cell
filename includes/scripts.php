f47e91ff34a866ec1346e7569869f220a042453a
jamesl1001 <jamesl1001@msn.com>
2014-02-24 14:18:22 +0000

    Fix JS file name change

--- includes/scripts.php
+++ includes/scripts.php
@@ -18,7 +18,7 @@
             var latitude  = <?= $env->latitude; ?>;
             var longitude = <?= $env->longitude; ?>;
         </script>
-        <script src="/js/threejs.tweenjs.stats.loaders.controls.js"></script>
+        <script src="/js/threejs-tweenjs-stats-loaders-controls.js"></script>
         <script src="/js/env.js"></script>
     <?php endif; ?>
 
@@ -28,7 +28,7 @@
     <?php endif; ?>
 
     <?php if($isCapturing): ?>
-        <script src="/js/threejs.tweenjs.stats.loaders.controls.js"></script>
+        <script src="/js/threejs-tweenjs-stats-loaders-controls.js"></script>
         <script src="/js/capture-env.js"></script>
     <?php endif; ?>
 
@@ -54,6 +54,11 @@
     <?php if($isNewEnv): ?>
         <script src='https://maps.googleapis.com/maps/api/js?key=AIzaSyCNlx7Q6EFJ2nlJfkAnMIsCm94fdSzaqf4&sensor=false&libraries=places'></script>
         <script src='/build/new-env.min.js'></script>
+    <?php endif; ?>
+
+    <?php if($isCapturing): ?>
+        <script src="/build/threejs-tweenjs-stats-loaders-controls.min.js"></script>
+        <script src="/build/capture-env.min.js"></script>
     <?php endif; ?>
 
 <?php endif; ?>
