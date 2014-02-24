7942b6bc0ffcec78d126fcd84665ad46c907d180
faooful <faooful@gmail.com>
2014-02-24 00:53:00 +0000

    added a tween on the homepage between page flow and firststeps.

--- js/page-home.js
+++ js/page-home.js
@@ -1,4 +1,4 @@
-var primaryHeader = document.getElementById('primary-header');
+var primaryHeader   = document.getElementById('primary-header');
 var secondaryHeader = document.getElementById('secondary-header');
 primaryHeader.style.height = (window.innerHeight || document.documentElement.clientHeight) - 40 + 'px';
 secondaryHeader.style.marginTop = (parseInt(primaryHeader.style.height) + 40) + 'px';
@@ -7,3 +7,18 @@ window.onresize = function() {
     primaryHeader.style.height = (window.innerHeight || document.documentElement.clientHeight) - 40 + 'px';
     secondaryHeader.style.marginTop = (parseInt(primaryHeader.style.height) + 40) + 'px';
 };
+
+var pageFlow = document.getElementById('page-flow');
+
+pageFlow.addEventListener('click', function(e) {
+    e.preventDefault();
+    var position = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
+    var target   = document.getElementById('firststeps').offsetParent.offsetTop;
+    var timer = setInterval(function() {
+        window.scrollTo(0, position);
+        position += 50;
+        if(position >= target) {
+            clearInterval(timer);
+        }
+    }, 10);
+});
