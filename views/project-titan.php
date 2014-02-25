49e24b760fb090a366d3976a971cfbfba023057e
Jacob Hammond <ss_clan_cod@hotmail.com>
2014-02-25 14:44:25 +0000

    Added blockquote to project titan page

--- views/project-titan.php
+++ views/project-titan.php
@@ -7,15 +7,17 @@
     </header>
 
     <section>
-        <div class="align-centre">
-            <div class="section-padding half lgrey">
+        <div class="align-centre lgrey">
+            <div class="section-padding half">
                 <h1>REVIEWS</h1>
                 <p class="full-margin">Integer sit amet nibh sit amet lectus luctus placerat. Suspendisse eget arcu tortor. Fusce pretium sodales nisl et tempus.</p>
             </div>
-            <div class="section-padding half mblue">
-                <h1>QUOTE</h1>
-                <p class="full-margin">Integer sit amet nibh sit amet lectus luctus placerat. Suspendisse eget arcu tortor. Fusce pretium sodales nisl et tempus.</p>
-            </div>
+        <div class="section-padding half mblue">
+            <blockquote>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent massa libero, rhoncus at sodales et, fringilla vitae justo.</p>
+                <footer>Mr Person, Title Here</footer>
+            </blockquote>
+        </div>
         </div>
     </section>
 
