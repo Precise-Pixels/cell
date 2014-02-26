e124d091dd7f92269bd6a2ff1b84efdcb9df74d1
Jacob Hammond <ss_clan_cod@hotmail.com>
2014-02-26 02:10:20 +0000

    Removed old fb and twitter markup

--- includes/footer.php
+++ includes/footer.php
@@ -1,15 +1,6 @@
 <footer class="section-padding align-centre dgrey">
     <p id="copyright">&copy; Copyright Cell Industries 2150</p>
     <div id="social-icons">
-        <div id="btn--facebook" class="btn social">
-            <a href="#">
-                <div class="icon icon--facebook"></div>
-            </a>
-        </div>
-        <div id="btn--twitter" class="btn social">
-            <a href="#">
-                <div class="icon icon--twitter"></div>
-            </a>
-        </div>
+    
     </div>
     <a href="/" class="cell-logo ico-cell-logo"></a>
