4056ca4caa47ec852dc4425f5e1d181de95e3cf7
jamesl1001 <jamesl1001@msn.com>
2014-02-26 22:32:18 +0000

    Add webcam gestures to OrbitControls

--- views/environment.php
+++ views/environment.php
@@ -3,3 +3,7 @@
 <p><?= $env->timestamp; ?> | <?= $env->latitude; ?> | <?= $env->longitude; ?></p>
 
 <div id="env"></div>
+
+<video id="video" autoplay width="300" style="display:none"></video>
+<canvas id="canvas" style="width:300px;display:none;"></canvas>
+<canvas id="comp" style="width:300px;display:none;"></canvas>
