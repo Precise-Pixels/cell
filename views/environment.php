54780cd92f395a5f6eb9cb66a074c36fe32d6b05
jamesl1001 <jamesl1001@msn.com>
2014-02-26 23:49:50 +0000

    Add more browser compatibility. Make code 9000x more readable.

--- views/environment.php
+++ views/environment.php
@@ -4,6 +4,6 @@
 
 <div id="env"></div>
 
-<video id="video" autoplay width="300" style="display:none"></video>
-<canvas id="canvas" style="width:300px;display:none;"></canvas>
-<canvas id="comp" style="width:300px;display:none;"></canvas>
+<video id="video" autoplay width="300"></video>
+<canvas id="canvas" style="width:300px;"></canvas>
+<canvas id="comp" style="width:300px;"></canvas>
