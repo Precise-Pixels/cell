91373217dea15a0d303d5bcd1555b63e34013b46
jamesl1001 <jamesl1001@msn.com>
2014-02-27 15:54:23 +0000

    Add brightness/sensitivity radio toggles to gesture.js

--- js/gesture.js
+++ js/gesture.js
@@ -162,8 +162,38 @@ function test() {
 }
 
 var movethresh   = 2,
-    brightthresh = 300,
+    brightthresh = 600,
     overthresh   = 1000;
+
+var brightnessSetting = document.getElementsByName('brightness');
+var brightnessSettingChoice = 0;
+
+for(var i = 0; i < brightnessSetting.length; i++) {
+    brightnessSetting[i].addEventListener('click', function() {
+        brightnessSettingChanged();
+    }, false);
+}
+
+function brightnessSettingChanged() {
+    for(var i = 0; i < brightnessSetting.length; i++) {
+        if(brightnessSetting[i].checked == true) {
+            brightnessSettingChoice = i;
+        }
+    }
+
+    switch(brightnessSettingChoice) {
+        case 0:
+            brightthresh = 300;
+            break;
+        case 1:
+            brightthresh = 600;
+            break;
+        case 2:
+            brightthresh = 900;
+            break;
+    }
+}
+
 
 function calibrate() {
     wasdown = {
--- views/environment.php
+++ views/environment.php
@@ -4,6 +4,10 @@
 
 <div id="env"></div>
 
-<video id="video" autoplay width="300"></video>
+<video id="video" autoplay style="width:300px;"></video>
 <canvas id="canvas" style="width:300px;"></canvas>
 <canvas id="comp" style="width:300px;"></canvas>
+
+<label for="brightness">dark / more sensitive</label> <input type="radio" name="brightness" style="width:100px;">
+<label for="brightness">average</label> <input type="radio" name="brightness" checked style="width:100px;">
+<label for="brightness">bright / less sensitive</label> <input type="radio" name="brightness" style="width:100px;">
