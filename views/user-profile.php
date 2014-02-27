a1f8a0e3b04109f783e47c0f748778ca631f00a5
Jacob Hammond <ss_clan_cod@hotmail.com>
2014-02-27 18:56:01 +0000

    Added styles for profile pages

--- views/user-profile.php
+++ views/user-profile.php
@@ -1,3 +1,5 @@
+<main class="main--top">
+
     <section id="user-profile-banner" class="dgrey">
         <div class="align-centre">
             <div id="profile-image" class="imagebox quarter">
@@ -9,10 +11,12 @@
                 </figcaption>
             </div>
             <div id="user-info" class="half">
-                <h2><i class="ico-info"></i>Multimedia Designer</h2>
-                <h2><i class="ico-pin"></i>Canterbury, UK</h2>
+                <?php // If user is signed in and viewing their own profile
+                if(isset($_SESSION['userId']) && $_SESSION['userId'] == $user->userId): ?>
+                    <h2 class="user-profile-title"><i class="ico-my-cell"></i>Welcome back <?= $user->username; ?>!</h2>
+                <?php endif; ?>
                 <h2><i class="ico-env"></i><?= count($environments); ?> Environments Cloned</h2>
-                <h2><i class="ico-hash"></i>117 Profile Views</h2>
+                <h2><i class="ico-pin"></i>Canterbury, UK</h2>
                 <div id="user-social">
                     <a href="mailto:<?= $user->email; ?>"><i class="ico-email"></i></a>
                     <a href="#"><i class="ico-facebook"></i></a>
@@ -22,14 +26,13 @@
             <div id="user-buttons" class="quarter">
             <?php // If user is signed in and viewing their own profile
             if(isset($_SESSION['userId']) && $_SESSION['userId'] == $user->userId): ?>
-                <a href="#" class="btn"><i class="ico-env-new"></i></a>
+                <a href="<?= $user->username; ?>/env/new" class="btn" title="Clone New Environment"><i class="ico-env-new"></i></a>
+                <a href="/signout?r=<?= $_SERVER['REQUEST_URI']; ?>" class="btn" title="Sign Out"><i class="ico-logout"></i></a>
             <?php endif; ?>
             </div>
 
         </div>
     </section>
-
-<main id="user-profile-main">
 
     <section id="environment-listing" class="sdgrey">
         <div class="align-centre">
