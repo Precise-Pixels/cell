75a2f04477eb5b3b2e32ec65efef72cb9d70f237
Jacob Hammond <ss_clan_cod@hotmail.com>
2014-02-27 14:42:41 +0000

    Changed page title parallax speed, cleaned up js

--- js/header-animate.js
+++ js/header-animate.js
@@ -1,12 +1,12 @@
 window.onscroll = function (e) {
-    var headerTop = document.getElementById('top-bar');
-    var header = document.getElementById('fixed-header');
-    var hgroup   = header.getElementsByTagName('hgroup')[0];
-    var position = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
-    var target   = document.getElementsByTagName('section')[0].offsetParent.offsetTop;
+    var headerTop   = document.getElementById('top-bar');
+    var header      = document.getElementById('fixed-header');
+    var hgroup      = header.getElementsByTagName('hgroup')[0];
+    var position    = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
+    var target      = document.getElementsByTagName('section')[0].offsetParent.offsetTop;
 
-    header.style.top = position * -1 / 6 + "px";
-    hgroup.style.top = position * -1 / 3 + "px";
+    header.style.top = position * -1 / 2 + "px";
+    hgroup.style.top = position * -1 / 1 + "px";
     console.log(position);
 
     if ( position >= target ) {
