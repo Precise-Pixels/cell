f1cadb22f6986b591ef7dcae73f177039816ea64
jamesl1001 <jamesl1001@msn.com>
2014-02-27 16:05:23 +0000

    Move inline styles and flip video output

--- views/environment.php
+++ views/environment.php
@@ -4,9 +4,9 @@
 
 <div id="env"></div>
 
-<video id="video" autoplay style="width:300px;"></video>
-<canvas id="canvas" style="width:300px;"></canvas>
-<canvas id="comp" style="width:300px;"></canvas>
+<video id="video" autoplay></video>
+<canvas id="canvas"></canvas>
+<canvas id="comp"></canvas>
 
 <label for="brightness">dark / more sensitive</label> <input type="radio" name="brightness" style="width:100px;">
 <label for="brightness">average</label> <input type="radio" name="brightness" checked style="width:100px;">
