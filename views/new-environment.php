c4dfb9457dd384b47a03f48520b347f854ad6734
Jacob Hammond <ss_clan_cod@hotmail.com>
2014-02-28 16:53:26 +0000

    Rearranged markup, removed two text sections

--- views/new-environment.php
+++ views/new-environment.php
@@ -4,36 +4,15 @@
         header('location: /signin');
     }
 ?>
-<main class="main--top">
-
-    <header class="section-padding align-centre dgrey">
+    <header class="fixed-header section-padding align-centre dgrey">
         <hgroup class="align-vertical">
             <h1>CLONE A NEW ENVIRONMENT</h1>
         </hgroup>
     </header>
 
-    <section class="align-centre dgrey">
-        <ol class="align-centre">
-            <li class="third">
-                <h1>1</h1>
-                <img src="/img/new-env-instructions-1.png" class="scale">
-                <p>Zoom in fully to select an area of the map</p>
-            </li>
-            <li class="third">
-                <h1>2</h1>
-                <img src="/img/new-env-instructions-2.png" class="scale">
-                <p>Name your environment and click the clone button</p>
-            </li>
-            <li class="third">
-                <h1>3</h1>
-                <img src="/img/new-env-instructions-3.png" class="scale">
-                <p>View your newly cloned environment</p>
-            </li>
-        </ol>
-    </section>
+<main>
 
-
-    <section id="new-env-interface" class="section--spacer sdgrey">
+    <section id="new-env-interface" class="sdgrey">
         <div class="align-centre mblue">
             <input type="text" placeholder="Search Box" id="pac-input" class="controls">
             <div id="new-env-map"></div>
@@ -55,22 +34,27 @@
         </div>
     </section>
 
-    <section class="align-centre sdgrey">
-        <section class="section-content-wrapper">
-            <div class="half section-padding">
-                <h1>WHY ARE YOU HERE?</h1>
-                <div class="section-content-wrapper">
-                    <p>You are about to be part of Project Titan, helping us to preserve the planet. Suspendisse adipiscing nunc tortor, eget accumsan tellus tincidunt at. Ut suscipit elit id tellus tincidunt, ac luctus nisi consectetur proin velit libero.</p>
-                </div>
-            </div>
-            <div class="half section-padding sdgrey">
-                <h1>HOW WE DO IT</h1>
-                <div class="section-content-wrapper">
-                    <p>We send our Quantum Cell Clone Cubes from space. Praesent massa libero, rhoncus at sodales et, fringilla vitae justo. Suspendisse adipiscing nunc tortor, eget accumsan tellus tincidunt at. Ut suscipit elit id tellus tincidunt.</p>
-                </div>
-            </div>
-        </section>
-        
+    <section class="align-centre dgrey">
+        <ol class="align-centre">
+            <li class="third">
+                <h1>1</h1>
+                <img src="/img/new-env-instructions-1.png" class="scale">
+                <p>Zoom in fully to select an area of the map</p>
+            </li>
+            <li class="third">
+                <h1>2</h1>
+                <img src="/img/new-env-instructions-2.png" class="scale">
+                <p>Name your environment and click the clone button</p>
+            </li>
+            <li class="third">
+                <h1>3</h1>
+                <img src="/img/new-env-instructions-3.png" class="scale">
+                <p>View your newly cloned environment</p>
+            </li>
+        </ol>
+    </section>
+
+    <section class="align-centre sdgrey">   
         <section class="section-content-wrapper">
             <a href="technology" class="cta cta--example half">
                 <hgroup class="align-vertical">
