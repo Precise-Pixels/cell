9fa7cdb2161860bc94a17500762a53517da2e161
Jacob Hammond <ss_clan_cod@hotmail.com>
2014-03-02 16:02:50 +0000

    Added fixed header to recent clones page

--- views/recently-cloned-environments.php
+++ views/recently-cloned-environments.php
@@ -1,10 +1,10 @@
-<main>
-
-    <header class="section-padding align-centre dgrey">
+    <header class="fixed-header section-padding align-centre dgrey">
         <hgroup class="align-vertical">
             <h1>RECENTLY CLONED ENVIRONMENTS</h1>
         </hgroup>
     </header>
+
+<main>
 
     <section id="new-env-instructions" class="sdgrey section--spacer">
         <div class="align-centre section-padding">
@@ -20,7 +20,8 @@
                         <div class="imagebox quarter mblue">
                             <figure>
                                 <img src="/img/placeholder.gif" alt="<?= $env->name; ?>">
-                            </figure>
+<!--                                 <img src="/img/user/<?= $user->userId; ?>/capture-<?= $env->envId; ?>.jpg" alt="<?= $env->name; ?>">
+ -->                            </figure>
                             <figcaption>
                                 <h1><?= $env->name; ?></h1>
                             </figcaption>
