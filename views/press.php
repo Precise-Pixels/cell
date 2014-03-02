482a6143a97f74939a450a86645fdbd41593618f
faooful <faooful@gmail.com>
2014-03-02 19:16:34 +0000

    removed the name ids

--- views/press.php
+++ views/press.php
@@ -1,4 +1,4 @@
-<header class="fixed-header section-padding align-centre dgrey">
+<header id="fixed-header" class="fixed-header--pt section-padding dgrey">
     <hgroup class="align-vertical">
         <h1>PRESS</h1>
     </hgroup>
--- views/project-titan.php
+++ views/project-titan.php
@@ -1,6 +1,6 @@
 <header id="fixed-header" class="fixed-header--pt section-padding dgrey">
     <hgroup class="align-vertical">
-        <h1 id="name">PROJECT TITAN</h1>
+        <h1>PROJECT TITAN</h1>
     </hgroup>
 </header>
 
