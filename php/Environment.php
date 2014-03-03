f3bce740b9ee578c4de1b90cceb4570475ebb9a1
jamesl1001 <jamesl1001@msn.com>
2014-03-03 18:25:45 +0000

    Add link back to user's profile page on env page

--- php/Environment.php
+++ php/Environment.php
@@ -4,7 +4,7 @@ class Environment {
     static function getData($envId, $userId) {
         require('db.php');
 
-        $sth = $dbh->query("SELECT userId, timestamp, latitude, longitude, elevationString, name FROM environments WHERE envId='$envId' AND userId='$userId'");
+        $sth = $dbh->query("SELECT environments.userId, environments.timestamp, latitude, longitude, elevationString, name, username FROM environments INNER JOIN users ON environments.userId=users.userId WHERE environments.envId='$envId' AND environments.userId='$userId'");
         $sth->setFetchMode(PDO::FETCH_OBJ);
         $result = $sth->fetch();
 
--- views/environment.php
+++ views/environment.php
@@ -2,4 +2,6 @@
 
 <p><?= $env->timestamp; ?> | <?= $env->latitude; ?> | <?= $env->longitude; ?></p>
 
+<a href="/user/<?= $env->username ?>"><?= $env->username ?>'s profile</a>
+
 <div id="model"><img src="/img/placeholder.gif" alt="<?= $env->name; ?>"/></div>
