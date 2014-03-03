16a67a8feb05b1f1d5ba815aa069457e67139c22
faooful <faooful@gmail.com>
2014-03-03 18:43:40 +0000

    created a new variable for primary-header.

--- js/header-animate.js
+++ js/header-animate.js
@@ -1,12 +1,14 @@
 window.onscroll = function (e) {
     var headerTop   = document.getElementById('top-bar');
-    var header      = document.querySelectorAll('#fixed-header, #primary-header');
+    var header      = document.getElementById('fixed-header');
+    var prim-header = document.getElementById('primary-header');
     var hgroup      = header.getElementsByTagName('hgroup')[0];
     var position    = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
     var target      = document.getElementsByTagName('section')[0].offsetParent.offsetTop;
 
-    header.style.top = position * -1 / 3 + "px";
-    hgroup.style.top = position * -1 / 2 + "px";
+    header.style.top      = position * -1 / 3 + "px";
+    prim-header.style.top = position * -1 / 3 + "px";
+    hgroup.style.top      = position * -1 / 2 + "px";
     console.log(position);
 
     if ( position >= target ) {
