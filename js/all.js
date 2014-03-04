148d5ddb84055257b703a6b1aec073cec3b29ecc
jamesl1001 <jamesl1001@msn.com>
2014-03-04 23:53:04 +0000

    Remove optional parameter and add comment

--- js/all.js
+++ js/all.js
@@ -1,3 +1,4 @@
+// Navigation overlay
 var navOverlay    = document.getElementById('nav-overlay');
 var siteNavToggle = document.getElementById('site-nav-toggle');
 var userNavToggle = document.getElementById('user-nav-toggle');
@@ -5,4 +6,4 @@ var userNavToggle = document.getElementById('user-nav-toggle');
 navOverlay.addEventListener('click', function() {
     siteNavToggle.checked = false;
     userNavToggle.checked = false;
-}, false);
+});
