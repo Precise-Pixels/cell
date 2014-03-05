5596b3cfe9c35b4f41096c5b6c4836b1485ba598
jamesl1001 <jamesl1001@msn.com>
2014-03-05 22:25:32 +0000

    Compress satellite texture as jpg instead of png

--- php/getEnvTexture.php
+++ php/getEnvTexture.php
@@ -5,6 +5,6 @@ $lon = $_GET['lon'];
 
 $img = imagecreatefrompng("http://maps.googleapis.com/maps/api/staticmap?center=$lat,$lon&zoom=11&size=640x640&scale=2&maptype=satellite&sensor=false&key=AIzaSyCNlx7Q6EFJ2nlJfkAnMIsCm94fdSzaqf4");
 
-header('Content-Type: image/png');
-imagepng($img);
+header('Content-Type: image/jpeg');
+imagejpeg($img);
 imagedestroy($img);
