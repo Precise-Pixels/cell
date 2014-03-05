9d8fd8e3185760c2219cd491ee77e0b734d616bc
Jacob Hammond <ss_clan_cod@hotmail.com>
2014-03-05 19:25:54 +0000

    Added initial header styles for environment page

--- views/environment.php
+++ views/environment.php
@@ -1,21 +1,31 @@
-<h1>ENV</h1>
+<main class="env">
 
-<progress id="prog-bar" min=0 max=100 value=0></progress>
+    <header class="sdgrey">
+        <div id="env-user-badge">
+            <h2>Cloned by:</h2>
+            <a href="/user/<?= $env->username ?>"><?= $env->username ?>'s profile</a>
+        </div>
+        <hgroup class="align-vertical">
+            <h1><i class="ico-env"></i><?= $env->name; ?></h1>
+            <h2><i class="ico-pin"></i><?= $env->latitude; ?>, <?= $env->longitude; ?></h2>
+        </hgroup>
+    </header>
+    <?= $env->timestamp; ?> 
+    <progress id="prog-bar" min=0 max=100 value=0></progress>
 
-<p><?= $env->timestamp; ?> | <?= $env->latitude; ?> | <?= $env->longitude; ?></p>
 
-<a href="/user/<?= $env->username ?>"><?= $env->username ?>'s profile</a>
+    <div id="model"><img src="/img/placeholder.gif" alt="<?= $env->name; ?>"/></div>
 
-<div id="model"><img src="/img/placeholder.gif" alt="<?= $env->name; ?>"/></div>
+    <div id="env"></div>
 
-<div id="env"></div>
+    <div id="env-data"></div>
 
-<div id="env-data"></div>
+    <video id="video" autoplay></video>
+    <canvas id="canvas"></canvas>
+    <canvas id="comp"></canvas>
 
-<video id="video" autoplay></video>
-<canvas id="canvas"></canvas>
-<canvas id="comp"></canvas>
+    <label for="sensitivity">more sensitive</label> <input type="radio" name="sensitivity">
+    <label for="sensitivity">average</label> <input type="radio" name="sensitivity" checked>
+    <label for="sensitivity">less sensitive</label> <input type="radio" name="sensitivity">
 
-<label for="sensitivity">more sensitive</label> <input type="radio" name="sensitivity">
-<label for="sensitivity">average</label> <input type="radio" name="sensitivity" checked>
-<label for="sensitivity">less sensitive</label> <input type="radio" name="sensitivity">
+</main>
