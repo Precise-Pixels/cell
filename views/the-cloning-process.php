aeb93c2b87aaad8347fbd3ecdf78226d44b5f016
faooful <faooful@gmail.com>
2014-03-06 19:24:40 +0000

    cube now decends.

--- views/the-cloning-process.php
+++ views/the-cloning-process.php
@@ -12,7 +12,7 @@
     <img src="/img/infographic/earth.png" class="ig-earth" data-0="top: 100%;transform:scale(1);" data-8000="top: 0%;transform: scale(1.2);"/>
     <img src="/img/infographic/satellite.png" class="ig-satellite" data-1000="top: 100%;" data-3000="top: -100%;"/>
 
-    <img src="/img/infographic/cube.png" class="ig-cube" data-5000="opacity: 1;" data-7000="opacity: 0;" data-7500="opacity: 0;" data-8500="opacity: 1;" data-20000=""/>
+    <img src="/img/infographic/cube.png" class="ig-cube" data-5000="opacity: 1;" data-7000="opacity: 0;" data-7500="opacity: 0;" data-8500="opacity: 1;" data-14000="top: 50%;" data-16000="top: 60%;" data-20000=""/>
     <img src="/img/infographic/cube-reentry.png" class="ig-cube-reentry" data-5000="opacity: 0;" data-6000="opacity: 1;" data-10000=""/>
 
     <div class="ig-overlay" data-6500="background-color:rgb(255,255,255);opacity: 0;" data-7500="background-color: rgb(255,255,255);opacity: 1;" data-10000="background-color: rgb(63,151,249);" data-14000=""></div>
@@ -30,9 +30,9 @@
     <img src="/img/infographic/mountain.jpg" class="ig-mountain" data-8000="top: 100%;" data-14000="top: 0%;"/>
     <img src="/img/infographic/plane.png" class="ig-plane" data-7500="left: -50%; top: 30%;" data-9000="left: 110%; top: -10%;"/>
 
-    <img src="/img/infographic/cloud.png" class="ig-cloud5" data-8500="top: 110%;" data-10000="top: 10%;" data-15000="top: 5%;" data-20000="top: 3%;"/>
-    <img src="/img/infographic/cloud.png" class="ig-cloud6" data-8500="top: 110%;" data-10000="top: 20%;" data-15000="top: 10%;" data-20000="top: 4%;"/>
-    <img src="/img/infographic/cloud.png" class="ig-cloud7" data-8500="top: 110%;" data-10000="top: 30%;" data-15000="top: 10%;" data-20000="top: 5%;"/>
+    <img src="/img/infographic/cloud.png" class="ig-cloud5" data-8500="top: 110%;" data-14000="top: 10%;" data-15000="top: 5%;" data-20000="top: 3%;"/>
+    <img src="/img/infographic/cloud.png" class="ig-cloud6" data-8500="top: 110%;" data-14000="top: 20%;" data-15000="top: 10%;" data-20000="top: 4%;"/>
+    <img src="/img/infographic/cloud.png" class="ig-cloud7" data-8500="top: 110%;" data-14000="top: 30%;" data-15000="top: 10%;" data-20000="top: 5%;"/>
     <img src="/img/infographic/mountain.jpg" class="ig-mountain-cover" data-13999="opacity: 0;" data-14000="opacity: 1;" data-20000/>
 
 </main>
