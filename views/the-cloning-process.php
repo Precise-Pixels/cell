b78dbdb9e1317bc11c25b1410da6889e5534ddff
faooful <faooful@gmail.com>
2014-03-07 01:30:09 +0000

    the reflection now lines up with the changed timings.

--- views/the-cloning-process.php
+++ views/the-cloning-process.php
@@ -12,7 +12,7 @@
     <img src="/img/infographic/earth.png" class="ig-earth" data-0="top:50%;transform:scale(1); filter:blur(3px);" data-5000="top:25%;transform:scale(1.1);filter:blur(0px);" data-8000="top:0%;transform:scale(1.3);"/>
     <img src="/img/infographic/satellite.png" class="ig-satellite" data-3000="top:100%;" data-5000="top:-300px;"/>
 
-    <img src="/img/infographic/cube.png" class="ig-cube" data-300="top:0%;" data-1000="top:50%;transform:scale(0.7)" data-5000="opacity:1;transform:scale(0.8)" data-8000="opacity:0;transform:scale(1)" data-8500="opacity: 1;" data-20000/>
+    <img src="/img/infographic/cube.png" class="ig-cube" data-300="top:0%;" data-1000="top:50%;transform:scale(0.7)" data-5000="opacity:1;transform:scale(0.8)" data-8000="opacity:0;transform:scale(1)" data-8500="opacity: 1;" data-13000="" data-20000/>
     <img src="/img/infographic/cube-reentry.png" class="ig-cube-reentry" data-5000="opacity:0;" data-8000="opacity:1;" data-10000=""/>
 
     <div class="ig-overlay" data-6500="background-color:rgb(255,255,255);opacity: 0;" data-7500="background-color: rgb(255,255,255);opacity: 1;" data-10000="background-color: rgb(63,151,249);" data-14000=""></div>
@@ -35,7 +35,7 @@
     <img src="/img/infographic/cloud.png" class="ig-cloud7" data-8500="top: 110%;" data-14000="top: 5%;" data-15000="top: 4%;" data-20000="top: 9%;"/>
     <img src="/img/infographic/mountain.jpg" class="ig-mountain-cover" data-13999="opacity: 0;" data-14000="opacity: 1;" data-20000/>
 
-    <img src="/img/infographic/cube-reflection.png" class="ig-cube-reflection" data-15000="opacity: 0; top: 85%;" data-16000="opacity: .2; top: 80%;" data-20000/>
+    <img src="/img/infographic/cube-reflection.png" class="ig-cube-reflection" data-13000="opacity: 0; top: 100%;" data-14000="opacity: .2; top: 85%;" data-20000/>
     <div class="ig-cube-scanner" data-16500="opacity: 0;" data-17000="opacity: 1;" data-20000="transform: perspective(800px) rotatey(0deg);"> </div>
 
 </main>
