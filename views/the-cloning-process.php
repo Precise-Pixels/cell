5551b5cd815b07a87152760f25b8e5e55d842e7a
Jacob Hammond <ss_clan_cod@hotmail.com>
2014-03-07 01:59:16 +0000

    Fixed conflicts

--- views/the-cloning-process.php
+++ views/the-cloning-process.php
@@ -12,23 +12,24 @@
     <img src="/img/infographic/earth.png" class="ig-earth" data-0="top:50%;transform:scale(1); filter:blur(3px);" data-5000="top:25%;transform:scale(1.1);filter:blur(0px);" data-8000="top:0%;transform:scale(1.3);"/>
     <img src="/img/infographic/satellite.png" class="ig-satellite" data-3000="top:100%;" data-5000="top:-300px;"/>
 
-    <img src="/img/infographic/cube.png" class="ig-cube" data-300="top:0%;" data-1000="top:50%;transform:scale(0.7)" data-5000="opacity:1;transform:scale(0.8)" data-8000="opacity:0;transform:scale(1)" data-8500="opacity: 1;" data-13000="" data-20000/>
-    <img src="/img/infographic/cube-reentry.png" class="ig-cube-reentry" data-5000="opacity:0;" data-8000="opacity:1;" data-10000=""/>
+    <img src="/img/infographic/cube.png" class="ig-cube" data-300="top:0%;" data-1000="top:50%;transform:scale(0.8)" data-5000="opacity:1;transform:scale(0.8)" data-8000="opacity:0;transform:scale(1)" data-8200="opacity:1;" data-20000=""/>
+    <img src="/img/infographic/cube-reentry.png" class="ig-cube-reentry" data-5000="opacity:0;" data-7000="opacity:1;" data-8500="opacity:0;"/>
 
-    <div class="ig-overlay" data-6500="background-color:rgb(255,255,255);opacity: 0;" data-7500="background-color: rgb(255,255,255);opacity: 1;" data-10000="background-color: rgb(63,151,249);" data-14000=""></div>
+    <img src="/img/infographic/above-clouds.png" class="ig-above-clouds" data-8000="opacity:0; top:0%;" data-8300="opacity:1; top:0%;" data-10000="opacity:1; top:-50%;" />
+    <div class="ig-overlay" data-7500="background-color:rgb(255,255,255);opacity: 0;" data-8000="background-color: rgb(255,255,255);opacity: 1;" data-10000="background-color: rgb(63,151,249);" data-14000=""></div>
+    
+    <img src="/img/infographic/cloud.png" class="ig-cloud" data-8500="top: 100%;" data-10000="top: -300%;"/>
+    <img src="/img/infographic/cloud.png" class="ig-cloud2" data-8500="top: 100%;" data-10000="top: -400%;"/>
+    <img src="/img/infographic/cloud.png" class="ig-cloud3" data-8500="top: 100%;" data-10000="top: -100%;"/>
+    <img src="/img/infographic/cloud.png" class="ig-cloud4" data-8500="top: 100%;" data-10000="top: -50%;"/>
+    <img src="/img/infographic/droplet.png" class="ig-droplet" data-8500="top: 100%;" data-10000="top: -50%;"/>
+    <img src="/img/infographic/droplet.png" class="ig-droplet2" data-8500="top: 100%;" data-10000="top: -300%;"/>
+    <img src="/img/infographic/droplet.png" class="ig-droplet3" data-8500="top: 100%;" data-10000="top: -400%;"/>
+    <img src="/img/infographic/droplet.png" class="ig-droplet4" data-8500="top: 100%;" data-10000="top: -100%;"/>
 
-    <img src="/img/infographic/cloud.png" class="ig-cloud" data-7500="top: 100%;" data-10000="top: -300%;"/>
-    <img src="/img/infographic/cloud.png" class="ig-cloud2" data-7500="top: 100%;" data-10000="top: -400%;"/>
-    <img src="/img/infographic/cloud.png" class="ig-cloud3" data-7500="top: 100%;" data-10000="top: -100%;"/>
-    <img src="/img/infographic/cloud.png" class="ig-cloud4" data-7500="top: 100%;" data-10000="top: -50%;"/>
-    <img src="/img/infographic/droplet.png" class="ig-droplet" data-7500="top: 100%;" data-10000="top: -50%;"/>
-    <img src="/img/infographic/droplet.png" class="ig-droplet2" data-7500="top: 100%;" data-10000="top: -300%;"/>
-    <img src="/img/infographic/droplet.png" class="ig-droplet3" data-7500="top: 100%;" data-10000="top: -400%;"/>
-    <img src="/img/infographic/droplet.png" class="ig-droplet4" data-7500="top: 100%;" data-10000="top: -100%;"/>
 
-
-    <img src="/img/infographic/mountain.jpg" class="ig-mountain" data-8000="top: 100%;" data-14000="top: 0%;"/>
-    <img src="/img/infographic/plane.png" class="ig-plane" data-7500="left: -50%; top: 30%;" data-9000="left: 110%; top: -10%;"/>
+    <img src="/img/infographic/mountain.jpg" class="ig-mountain" data-9000="top: 100%;" data-14000="top: 0%;"/>
+    <img src="/img/infographic/plane.png" class="ig-plane" data-8500="left: -50%; top: 30%;" data-9000="left: 110%; top: -10%;"/>
 
     <img src="/img/infographic/cloud.png" class="ig-cloud5" data-8500="top: 110%; left: 13%;" data-13000="top: 15%; left: 15%;" data-14000="top: 10%;" data-15000="top: 10%; left: 17%;" data-20000="left: 20%;"/>
     <img src="/img/infographic/cloud.png" class="ig-cloud6" data-8500="top: 110%; left: 42%;" data-13000="top: 15%; left: 45%;" data-14000="top: 10%;" data-15000="top: 10%; left: 47%;" data-20000="left: 50%;"/>
