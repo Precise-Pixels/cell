6f05a8e630133f7992b986f4f4be06cf36be63c7
Jacob Hammond <ss_clan_cod@hotmail.com>
2014-03-07 00:54:07 +0000

    Added some new images, played with timings

--- views/the-cloning-process.php
+++ views/the-cloning-process.php
@@ -6,14 +6,15 @@
 
 <main>
 
+    <img src="/img/infographic/bg.jpg" class="ig-background" data-0="top:0%;" data-8000="top:-10%;"/>
+
+    <img src="/img/infographic/moon.png" class="ig-moon" data-700="top:0%;" data-3000="top:-60%;"/>
+    <img src="/img/infographic/earth.png" class="ig-earth" data-0="top:50%;transform:scale(1); filter:blur(3px);" data-5000="top:25%;transform:scale(1.1);filter:blur(0px);" data-8000="top:0%;transform:scale(1.3);"/>
+    <img src="/img/infographic/satellite.png" class="ig-satellite" data-3000="top:100%;" data-5000="top:-300px;"/>
+
+    <img src="/img/infographic/cube.png" class="ig-cube" data-300="top:0%;" data-1000="top:50%;transform:scale(0.7)" data-5000="opacity:1;transform:scale(0.8)" data-8000="opacity:0;transform:scale(1)"/>
+    <img src="/img/infographic/cube-reentry.png" class="ig-cube-reentry" data-5000="opacity:0;" data-8000="opacity:1;" data-10000=""/>
     <img src="/img/infographic/bg.jpg" class="ig-background" data-0="top: 0%;" data-10000="top: -300%;"/>
-
-    <img src="/img/infographic/moon.png" class="ig-moon" data-0="top: 0%;" data-2000="top: -60%;"/>
-    <img src="/img/infographic/earth.png" class="ig-earth" data-0="top: 100%;transform:scale(1);" data-8000="top: 0%;transform: scale(1.2);"/>
-    <img src="/img/infographic/satellite.png" class="ig-satellite" data-1000="top: 100%;" data-3000="top: -100%;"/>
-
-    <img src="/img/infographic/cube.png" class="ig-cube" data-5000="opacity: 1;" data-7000="opacity: 0;" data-7500="opacity: 0;" data-8500="opacity: 1;" data-14000="top: 50%;" data-16000="top: 60%;" data-20000=""/>
-    <img src="/img/infographic/cube-reentry.png" class="ig-cube-reentry" data-5000="opacity: 0;" data-6000="opacity: 1;" data-10000=""/>
 
     <div class="ig-overlay" data-6500="background-color:rgb(255,255,255);opacity: 0;" data-7500="background-color: rgb(255,255,255);opacity: 1;" data-10000="background-color: rgb(63,151,249);" data-14000=""></div>
 
