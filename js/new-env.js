85077ab65a4fb4e404f8d850b23ece754dc0f0b3
jamesl1001 <jamesl1001@msn.com>
2014-03-10 16:22:53 +0000

    Prevent env from being re-cloned if user presses Back on env page; redirect to new-env instead.

--- js/new-env.js
+++ js/new-env.js
@@ -1,3 +1,5 @@
+document.cookie = 'preventRecapture=false; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/';
+
 // Thanks: http://www.maptiler.org/google-maps-coordinates-tile-bounds-projection/
 var map;
 var currentTile;
