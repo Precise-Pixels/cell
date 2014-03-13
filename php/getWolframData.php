f92015e95a4350aec83f2350b2e93e90310dcdb0
jamesl1001 <jamesl1001@msn.com>
2014-03-13 00:52:17 +0000

    Parse certain pods to improve readability of plaintext data

--- php/getWolframData.php
+++ php/getWolframData.php
@@ -35,6 +35,52 @@ if($hasData == 'true') {
     $dataCategories = array_map('array_filter', $dataCategories);
     $dataCategories = array_filter($dataCategories);
 
+    // Parse certain pods to improve readability
+    function parsePlainText($key, $pod) {
+        $plaintext = $pod[0]->subpod->plaintext;
+
+        switch($key) {
+            case 'nearbyCities2':
+                $replaceWith = '</li><li>';
+                $i           = 0;
+
+                $plaintext = preg_replace('/\(straight-line distance to city centers\)/', '', $plaintext);
+                preg_match_all('/people/', $plaintext, $matches, PREG_OFFSET_CAPTURE);
+
+                foreach($matches[0] as $match) {
+                    if($i == count($matches[0]) - 1) { continue; }
+                    $plaintext = substr_replace($plaintext, $replaceWith, strlen($match[0]) + $match[1] + 1 + $i * strlen($replaceWith), 0);
+                    $i++;
+                }
+
+                return '<ul><li>' . $plaintext . '</li></ul>';
+                break;
+
+            case 'nearbyFeatures':
+                $replaceWith = '</li><li>';
+                $i           = 0;
+
+                $plaintext = preg_replace('/\(straight-line distance to feature center\)/', '', $plaintext);
+                preg_match_all('/miles [a-zA-Z0-9\-]+/', $plaintext, $matches, PREG_OFFSET_CAPTURE);
+
+                foreach($matches[0] as $match) {
+                    if($i == count($matches[0]) - 1) { continue; }
+                    $plaintext = substr_replace($plaintext, $replaceWith, strlen($match[0]) + $match[1] + 1 + $i * strlen($replaceWith), 0);
+                    $i++;
+                }
+
+                return '<ul><li>' . $plaintext . '</li></ul>';
+                break;
+
+            case 'localWeather':
+                return preg_replace('/\|/', ',', $plaintext);
+                break;
+
+            default:
+                return $plaintext;
+        }
+    }
+
     foreach($dataCategories as $key=>$categoryPods) {
         switch($key) {
             case 'categoryNearby':
@@ -58,8 +104,8 @@ if($hasData == 'true') {
         
             echo '<h1><i class=" '. $icon .' "></i>' . $title . '</h1>';
         
-        foreach($categoryPods as $pod) {
-            echo '<h2>' . $pod[0]->attributes()->title . '</h2><p>' . $pod[0]->subpod->plaintext . '<p>';
+        foreach($categoryPods as $key=>$pod) {
+            echo '<h2>' . $pod[0]->attributes()->title . '</h2><p>' . parsePlainText($key, $pod) . '</p>';
         }
 
         echo '</div>';
