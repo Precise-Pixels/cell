79b9de4b129b6fb4be325e5bf3718f01127561a9
faooful <faooful@gmail.com>
2014-03-16 16:00:45 +0000

    took out the unneeded half margin and changed the email to match the actaul email.

--- views/press.php
+++ views/press.php
@@ -10,10 +10,8 @@
         <div class="align-centre">
             <div class="half section-padding lgrey">
                 <h1><i class="ico-email"></i>CONTACT</h1>
-                <div class="half-margin">
-                    <h2>Press</h2>
-                    <a href="mailto:press@cell.dev">press@cell.dev</a>
-                </div>
+                    <p>For all press and media enquiries please contact:</p>
+                    <a href="mailto:press@cell-industries.co.uk">press@cell-industries.co.uk</a>
             </div>
             <div class="half">
                 <a href="/about" class="cta cta--about">
