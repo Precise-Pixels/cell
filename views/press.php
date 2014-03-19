4efaf545b4d9ca775108fad420346cbad87bc8ea
Jacob Hammond <ss_clan_cod@hotmail.com>
2014-03-19 00:28:15 +0000

    Removed CTA from top of press page, doesn't work on lower breakpoints

--- views/press.php
+++ views/press.php
@@ -8,18 +8,10 @@
 
     <section>
         <div class="align-centre">
-            <div class="half section-padding lgrey">
+            <div class="section-padding lgrey">
                 <h1><i class="ico-email"></i>CONTACT</h1>
                 <p>For all enquiries please contact:</p>
                 <a href="mailto:press@cell-industries.co.uk">press@cell-industries.co.uk</a>
-            </div>
-            <div class="half mobile-hide">
-                <a href="/about" id="press-page-cta" class="cta cta--about">
-                    <hgroup class="align-vertical">
-                        <h1>About Cell</h1>
-                        <h2>Find out about Cell Industries</h2>
-                    </hgroup>
-                </a>
             </div>
         </div>
     </section>
