cceb9b0bd46cff2b5376be49044eda15508e44e6
jamesl1001 <jamesl1001@msn.com>
2014-03-19 18:50:26 +0000

    Add sections and images

--- views/the-cloning-process.php
+++ views/the-cloning-process.php
@@ -1,4 +1,8 @@
 <main>
 
+    <section class="ig-section ig-section--1"></section>
+    <section class="ig-section ig-section--2"></section>
+    <section class="ig-section ig-section--3"></section>
+    <section class="ig-section ig-section--4"></section>
 
 </main>
