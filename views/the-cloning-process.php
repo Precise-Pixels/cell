cb18073a20feb43ce47107e385aec324d1844654
jamesl1001 <jamesl1001@msn.com>
2014-03-19 21:03:46 +0000

    Add animated loop of plane

--- views/the-cloning-process.php
+++ views/the-cloning-process.php
@@ -2,12 +2,21 @@
 
     <div id="ig-slide-wrapper">
         <div id="ig-slides" class="ig-current-slide--1 ig-transition-complete">
-            <img src="/img/infographic/1.jpg" alt=""/>
-            <img src="/img/infographic/2.jpg" alt=""/>
-            <img src="/img/infographic/3.jpg" alt=""/>
-            <img src="/img/infographic/4.jpg" alt=""/>
+            <div class="ig-slide">
+                <img src="/img/infographic/1.jpg" alt="" class="ig-slide-background"/>
+            </div>
+            <div class="ig-slide">
+                <img src="/img/infographic/2.jpg" alt="" class="ig-slide-background"/>
+            </div>
+            <div class="ig-slide">
+                <img src="/img/infographic/3.jpg" alt="" class="ig-slide-background"/>
+                <img src="/img/infographic/plane.jpg" alt="Plane" id="ig-plane" class="ig-animate"/>
+            </div>
+            <div class="ig-slide">
+                <img src="/img/infographic/4.jpg" alt="" class="ig-slide-background"/>
+            </div>
         </div>
-        <img id="ig-cube" alt="Clone Cube"/>
+        <img src="/img/infographic/cube.jpg" id="ig-cube" alt="Clone Cube"/>
         <i id="ig-prev" class="ico-arrow-up2"></i>
         <i id="ig-next" class="ico-arrow-down2"></i>
     </div>
