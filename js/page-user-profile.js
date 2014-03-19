523c0dc78fb2e71ea717913c4d54ab2150d34850
jamesl1001 <jamesl1001@msn.com>
2014-03-19 01:06:43 +0000

    Shorten variable names from 'userProfile' to 'up'

--- js/page-user-profile.js
+++ js/page-user-profile.js
@@ -1,27 +1,28 @@
-// Edit profile
-var userProfileEdit          = document.getElementById('user-profile-edit');
-var userProfileUsername      = document.getElementById('user-profile-username');
-var userProfileLocation      = document.getElementById('user-profile-location');
-var userProfileLocationInput = document.getElementById('user-profile-location-input');
-var userProfileFacebookInput = document.getElementById('user-profile-facebook-input');
-var userProfileTwitterInput  = document.getElementById('user-profile-twitter-input');
+// Edit user profile
+// up == user profile
+var upEdit          = document.getElementById('user-profile-edit');
+var upUsername      = document.getElementById('user-profile-username');
+var upLocation      = document.getElementById('user-profile-location');
+var upLocationInput = document.getElementById('user-profile-location-input');
+var upFacebookInput = document.getElementById('user-profile-facebook-input');
+var upTwitterInput  = document.getElementById('user-profile-twitter-input');
 
 var editing = false;
-var userProfileUsernameCurrent = userProfileUsername.innerHTML;
-var userProfileLocationCurrent = userProfileLocation.innerHTML;
-var href = userProfileFacebookInput.previousSibling.href.split('/');
-var userProfileFacebookCurrent = href[href.length - 1];
-var href = userProfileTwitterInput.previousSibling.href.split('/');
-var userProfileTwitterCurrent  = href[href.length - 1];
+var upUsernameCurrent = upUsername.innerHTML;
+var upLocationCurrent = upLocation.innerHTML;
+var href              = upFacebookInput.previousSibling.href.split('/');
+var upFacebookCurrent = href[href.length - 1];
+var href              = upTwitterInput.previousSibling.href.split('/');
+var upTwitterCurrent  = href[href.length - 1];
 
-userProfileEdit.addEventListener('click', function(e) {
+upEdit.addEventListener('click', function(e) {
     if(editing) {
         // Save
         // Only send the request if something has changed
-        if(userProfileLocationInput.value == userProfileLocationCurrent && userProfileFacebookInput.value == userProfileFacebookCurrent && userProfileTwitterInput.value == userProfileTwitterCurrent) {
+        if(upLocationInput.value == upLocationCurrent && upFacebookInput.value == upFacebookCurrent && upTwitterInput.value == upTwitterCurrent) {
             resetForm();
         } else {
-            var data = 'location=' + userProfileLocationInput.value + '&facebook=' + userProfileFacebookInput.value + '&twitter=' + userProfileTwitterInput.value;
+            var data = 'location=' + upLocationInput.value + '&facebook=' + upFacebookInput.value + '&twitter=' + upTwitterInput.value;
             var request = new XMLHttpRequest;
             request.open('POST', '/php/saveUserProfile.php', true);
             request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
@@ -44,12 +45,12 @@ userProfileEdit.addEventListener('click', function(e) {
             e.target.firstChild.className = 'ico-edit';
 
             // Reinstate username
-            userProfileUsername.innerHTML = userProfileUsernameCurrent;
+            upUsername.innerHTML = upUsernameCurrent;
 
             // Hide input fields
-            userProfileLocationInput.className += ' user-profile-input--hidden';
-            userProfileFacebookInput.className += ' user-profile-input--hidden';
-            userProfileTwitterInput.className  += ' user-profile-input--hidden';
+            upLocationInput.className += ' user-profile-input--hidden';
+            upFacebookInput.className += ' user-profile-input--hidden';
+            upTwitterInput.className  += ' user-profile-input--hidden';
         }
     } else {
         // Edit
@@ -57,25 +58,25 @@ userProfileEdit.addEventListener('click', function(e) {
         e.target.firstChild.className = 'ico-save';
 
         // Show link to Gravatar
-        userProfileUsername.innerHTML = '<a href="http://gravatar.com/" target="_blank" title="Change your avatar at Gravatar.com"><i class="ico-edit"></i>Gravatar.com</a>';
+        upUsername.innerHTML = '<a href="http://gravatar.com/" target="_blank" title="Change your avatar at Gravatar.com"><i class="ico-edit"></i>Gravatar.com</a>';
 
         // Show input fields
-        userProfileLocationInput.className = userProfileLocationInput.className.replace(' user-profile-input--hidden', '');
-        userProfileFacebookInput.className = userProfileFacebookInput.className.replace(' user-profile-input--hidden', '');
-        userProfileTwitterInput.className  = userProfileTwitterInput.className.replace(' user-profile-input--hidden', '');
+        upLocationInput.className = upLocationInput.className.replace(' user-profile-input--hidden', '');
+        upFacebookInput.className = upFacebookInput.className.replace(' user-profile-input--hidden', '');
+        upTwitterInput.className  = upTwitterInput.className.replace(' user-profile-input--hidden', '');
 
         // If location has already been entered, put it into the input field
-        if(userProfileLocation.innerHTML != 'No location details') {
-            userProfileLocationInput.value = userProfileLocationCurrent;
+        if(upLocation.innerHTML != 'No location details') {
+            upLocationInput.value = upLocationCurrent;
         }
 
         // If Facebook/Twitter usernames have already been entered, put them into the input fields
-        if(userProfileFacebookInput.previousSibling.href.slice(-1) != '#') {
-            userProfileFacebookInput.value = userProfileFacebookCurrent;
+        if(upFacebookInput.previousSibling.href.slice(-1) != '#') {
+            upFacebookInput.value = upFacebookCurrent;
         }
 
-        if(userProfileTwitterInput.previousSibling.href.slice(-1) != '#') {
-            userProfileTwitterInput.value = userProfileTwitterCurrent;
+        if(upTwitterInput.previousSibling.href.slice(-1) != '#') {
+            upTwitterInput.value = upTwitterCurrent;
         }
     }
 });
