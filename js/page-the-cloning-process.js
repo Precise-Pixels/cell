14518ccae169badfb7c79ff0511d7278f00e18dc
jamesl1001 <jamesl1001@msn.com>
2014-03-20 13:53:08 +0000

    Change infographic back to covering the entire window

--- js/page-the-cloning-process.js
+++ js/page-the-cloning-process.js
@@ -1,22 +1,3 @@
-// Calculate the dimensions of a square that always fits in the viewport (with added padding)
-var igSlideWrapper = document.getElementById('ig-slide-wrapper');
-calculateSquare();
-
-window.addEventListener('resize', function() {
-    calculateSquare();
-});
-
-function calculateSquare() {
-    var windowWidth  = (window.innerWidth || document.documentElement.clientWidth);
-    var windowHeight = (window.innerHeight || document.documentElement.clientHeight) - 40;
-
-    if(windowWidth > windowHeight) {
-        igSlideWrapper.style.width = igSlideWrapper.style.height = windowHeight - 120 + 'px';
-    } else {
-        igSlideWrapper.style.width = igSlideWrapper.style.height = windowWidth - 80 + 'px';
-    }
-}
-
 // Handle interaction
 var igSlides = document.getElementById('ig-slides');
 var igPrev   = document.getElementById('ig-prev');
--- views/the-cloning-process.php
+++ views/the-cloning-process.php
@@ -1,24 +1,22 @@
 <main>
 
-    <div id="ig-slide-wrapper">
-        <div id="ig-slides" class="ig-current-slide--1 ig-transition-complete">
-            <div class="ig-slide">
-                <img src="/img/infographic/1.jpg" alt="" class="ig-slide-background"/>
-            </div>
-            <div class="ig-slide">
-                <img src="/img/infographic/2.jpg" alt="" class="ig-slide-background"/>
-            </div>
-            <div class="ig-slide">
-                <img src="/img/infographic/3.jpg" alt="" class="ig-slide-background"/>
-                <img src="/img/infographic/plane.jpg" alt="Plane" id="ig-plane" class="ig-animate"/>
-            </div>
-            <div class="ig-slide">
-                <img src="/img/infographic/4.jpg" alt="" class="ig-slide-background"/>
-            </div>
+    <div id="ig-slides" class="ig-current-slide--1 ig-transition-complete">
+        <div class="ig-slide">
+            <div class="ig-slide ig-slide--1"></div>
         </div>
-        <img src="/img/infographic/cube.jpg" id="ig-cube" alt="Clone Cube"/>
-        <i id="ig-prev" class="ico-arrow-up2"></i>
-        <i id="ig-next" class="ico-arrow-down2"></i>
+        <div class="ig-slide">
+            <div class="ig-slide ig-slide--2"></div>
+        </div>
+        <div class="ig-slide">
+            <div class="ig-slide ig-slide--3"></div>
+            <img src="/img/infographic/plane.jpg" alt="Plane" id="ig-plane" class="ig-animate"/>
+        </div>
+        <div class="ig-slide">
+            <div class="ig-slide ig-slide--4"></div>
+        </div>
     </div>
+    <img src="/img/infographic/cube.jpg" id="ig-cube" alt="Clone Cube"/>
+    <i id="ig-prev" class="ico-arrow-up2"></i>
+    <i id="ig-next" class="ico-arrow-down2"></i>
 
 </main>
