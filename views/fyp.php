15f99822fe4792399522c5d8074e520088c39c77
jamesl1001 <jamesl1001@msn.com>
2014-03-20 21:56:36 +0000

    Add FYP page

---
+++ views/fyp.php
@@ -0,0 +1,18 @@
+<header id="fixed-header" class="fixed-header--fyp section-padding">
+    <hgroup class="align-vertical">
+        <h1>FINAL YEAR PROJECT</h1>
+        <?php require_once('includes/balls.php'); ?>
+    </hgroup>
+</header>
+
+<main>
+
+    <section>
+        <div class="section-padding align-centre lgrey">
+            <h1><i class="ico-company"></i>THE PROJECT</h1>
+            <p class="full">Cell Industries is a Multimedia Technology and Design University of Kent final year project by <a href="http://jacobhammond.co.uk/" target="_blank">Jacob Hammond</a>, <a href="http://jalproductions.co.uk/" target="_blank">James Lee</a> and <a href="http://faooful.com/" target="_blank">Joseph Williams</a>.</p>
+            <p class="full">More information coming soon.</p>
+        </div>
+    </section>
+
+</main>
