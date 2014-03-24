2190bcabcbe04eb281ae09b515c52edf553dc2b4
Jacob Hammond <ss_clan_cod@hotmail.com>
2014-03-24 01:39:11 +0000

    Added cube and fire effect

--- views/the-cloning-process.php
+++ views/the-cloning-process.php
@@ -17,7 +17,8 @@
             <div id="ig-scanner-wrapper" class="ig-animate"><img src="/img/infographic/scanner.png" alt="Scanner" id="ig-scanner"/></div>
         </div>
     </div>
-    <img src="/img/infographic/cube.jpg" id="ig-cube" alt="Clone Cube"/>
+    <img src="/img/infographic/clone-cube.png" id="ig-cube" alt="Clone Cube"/>
+    <img src="/img/infographic/clone-cube-fire.png" alt="Plane" id="ig-cube-fire" class="ig-animate"/>
     <i id="ig-prev" class="ico-arrow-up2"></i>
     <i id="ig-next" class="ico-arrow-down2"></i>
 
