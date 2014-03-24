d01b5e9375323bae6041273c9eccfbea864ab62d
faooful <faooful@gmail.com>
2014-03-24 14:15:35 +0000

    added a section to the 404 page and put a message in a p tag.

--- views/404.php
+++ views/404.php
@@ -1,9 +1,15 @@
 <header id="fixed-header" class="fixed-header--signin section-padding">
     <hgroup class="align-vertical">
-        <h1>404</h1>
+        <h1>404: PAGE NOT FOUND</h1>
     </hgroup>
 </header>
 
 <main>
+
+        <section>
+        <div class="section-padding align-centre lgrey">
+            <p>Oops! Sorry, but the page you are looking for has not been found. Try checking the url for errors, then hit the refresh button on your browser.</p>
+        </div>
+    </section>
 
 </main>
