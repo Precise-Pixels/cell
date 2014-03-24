7d9fd4cbd756c1db609b00c269bae6f580de1804
faooful <faooful@gmail.com>
2014-03-24 14:12:25 +0000

    added parallax to the 404 page.

--- views/404.php
+++ views/404.php
@@ -1,9 +1,9 @@
-<main>
+<header id="fixed-header" class="fixed-header--signin section-padding">
+    <hgroup class="align-vertical">
+        <h1>404</h1>
+    </hgroup>
+</header>
 
-    <header class="section-padding align-centre dgrey">
-        <hgroup class="align-vertical">
-            <h1>404</h1>
-        </hgroup>
-    </header>
+<main>
 
 </main>
