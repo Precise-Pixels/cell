c61eda11a8ac48fed3af1c0477f1f9a7eef482c5
jamesl1001 <jamesl1001@msn.com>
2014-03-24 18:39:07 +0000

    Remove closing i tags

--- views/privacy-policy.php
+++ views/privacy-policy.php
@@ -10,42 +10,42 @@
     <section>
         <div class="section-padding align-centre lgrey">
             <div class="section-content-wrapper half-margin">
-                <h1></i>INFORMATION WE COLLECT</h1>
+                <h1>INFORMATION WE COLLECT</h1>
                 <p>...</p>
             </div>
 
             <div class="section-content-wrapper half-margin">
-                <h1></i>USE OF COOKIES</h1>
+                <h1>USE OF COOKIES</h1>
                 <p>...</p>
             </div>
 
             <div class="section-content-wrapper half-margin">
-                <h1></i>USE OF YOUR INFORMATION</h1>
+                <h1>USE OF YOUR INFORMATION</h1>
                 <p>...</p>
             </div>
 
             <div class="section-content-wrapper half-margin">
-                <h1></i>STORING YOUR PERSONAL DATA</h1>
+                <h1>STORING YOUR PERSONAL DATA</h1>
                 <p>...</p>
             </div>
 
             <div class="section-content-wrapper half-margin">
-                <h1></i>DISCLOSING YOUR PERSONAL INFORMATION</h1>
+                <h1>DISCLOSING YOUR PERSONAL INFORMATION</h1>
                 <p>...</p>
             </div>
 
             <div class="section-content-wrapper half-margin">
-                <h1></i>THIRD PARTY LINKS</h1>
+                <h1>THIRD PARTY LINKS</h1>
                 <p>...</p>
             </div>
 
             <div class="section-content-wrapper half-margin">
-                <h1></i>ACCESS TO INFORMATION</h1>
+                <h1>ACCESS TO INFORMATION</h1>
                 <p>...</p>
             </div>
 
             <div class="section-content-wrapper half-margin">
-                <h1></i>CONTACT US</h1>
+                <h1>CONTACT US</h1>
                 <p>...</p>
             </div>
         </div>
