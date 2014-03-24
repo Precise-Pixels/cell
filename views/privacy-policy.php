5ec592d2270d3a4e6fff32ebd227ab8a2d150a52
jamesl1001 <jamesl1001@msn.com>
2014-03-24 19:26:55 +0000

    Add Privacy Policy copy

--- views/privacy-policy.php
+++ views/privacy-policy.php
@@ -9,45 +9,64 @@
 
     <section>
         <div class="section-padding align-centre lgrey">
-            <div class="section-content-wrapper half-margin">
-                <h1>INFORMATION WE COLLECT</h1>
-                <p>...</p>
-            </div>
+            <h1>INFORMATION WE COLLECT</h1>
+            <p>In running and maintaining our website we may collect and process the following data about you:</p>
+            <ul>
+                <li>Information about your use of our site including details of your visits such as pages viewed and the resources that you access. Such information includes but is not exclusive to traffic data, location data and other communication data.</li>
+                <li>Information provided voluntarily by you. For example, when you register for MyCell or enter your details on your profile page.</li>
+                <li>Information that you provide when you communicate with us by any means.</li>
+            </ul>
+        </div>
 
-            <div class="section-content-wrapper half-margin">
-                <h1>USE OF COOKIES</h1>
-                <p>...</p>
-            </div>
+        <div class="section-padding align-centre lgrey">
+            <h1>USE OF COOKIES</h1>
+            <p>When using our site, we may set cookies on your computer in order to assist with providing you a great user experience.</p>
+            <p>We or our third-party providers may set the following cookies whilst using our site:</p>
+            <ul>
+                <li>preventRecapture - set by us to ensure errors do not occur when capturing an environment</li>
+                <li>Session cookies - set by us in order to ensure that you remain signed in throughout the site</li>
+                <li>Google Analytics - set by Google in order to provide us with useful statitics regarding your visit</li>
+                <li>gravatar - set by Gravatar when loading in your avatar image from their service</li>
+            </ul>
+            <p>You can adjust the settings on your computer to decline any cookies if you wish. This can easily be done by activating the reject cookies setting on your computer.</p>
+        </div>
 
-            <div class="section-content-wrapper half-margin">
-                <h1>USE OF YOUR INFORMATION</h1>
-                <p>...</p>
-            </div>
+        <div class="section-padding align-centre lgrey">
+            <h1>USE OF YOUR INFORMATION</h1>
+            <p>We use the information that we collect from you to provide our services to you. The following is a breakdown of how your information is used:</p>
+            <ul>
+                <li>Your username is publically displayed on your profile, your cloned environments and in hyperlinks throughout the site</li>
+                <li>The location, Facebook and Twitter information you optionally provide us on your profile are only publically visible on your profile page</li>
+                <li>The name you give your environments are visible on the environment page itself, your profile page and on the Recently Cloned Environments page</li>
+                <li>Your email is never publically visible anywhere on the site</li>
+            </ul>
+        </div>
 
-            <div class="section-content-wrapper half-margin">
-                <h1>STORING YOUR PERSONAL DATA</h1>
-                <p>...</p>
-            </div>
+        <div class="section-padding align-centre lgrey">
+            <h1>STORING YOUR PERSONAL DATA</h1>
+            <p>All passwords are securely encrypted.</p>
+            <p>In operating our website it is necessary to transfer data that we collect from you to locations outside of the European Union for processing and storing. By providing your personal data to us, you agree to this transfer, storing or processing. We do our upmost to ensure that all reasonable steps are taken to make sure that your data is stored securely.</p>
+            <p>Unfortunately the sending of information via the internet is not totally secure and on occasion such information can be intercepted. We cannot guarantee the security of data that you choose to send us electronically. Sending such information is entirely at your own risk.</p>
+        </div>
 
-            <div class="section-content-wrapper half-margin">
-                <h1>DISCLOSING YOUR PERSONAL INFORMATION</h1>
-                <p>...</p>
-            </div>
+        <div class="section-padding align-centre lgrey">
+            <h1>DISCLOSING YOUR PERSONAL INFORMATION</h1>
+            <p>We will not disclose your personal information to any other party without your prior permission other than in accordance with this Privacy Policy and where we are legally required by law.</p>
+        </div>
 
-            <div class="section-content-wrapper half-margin">
-                <h1>THIRD PARTY LINKS</h1>
-                <p>...</p>
-            </div>
+        <div class="section-padding align-centre lgrey">
+            <h1>THIRD PARTY LINKS</h1>
+            <p>On occasion we include links to third parties on this website. Where we provide a link it does not mean that we endorse or approve that site’s policy towards visitor privacy. You should review their privacy policy before sending them any personal data.</p>
+        </div>
 
-            <div class="section-content-wrapper half-margin">
-                <h1>ACCESS TO INFORMATION</h1>
-                <p>...</p>
-            </div>
+        <div class="section-padding align-centre lgrey">
+            <h1>ACCESS TO INFORMATION</h1>
+            <p>In accordance with the Data Protection Act 1998 you have the right to access any information that we hold relating to you. Please note that we reserve the right to charge a fee of £10 to cover costs incurred by us in providing you with the information.</p>
+        </div>
 
-            <div class="section-content-wrapper half-margin">
-                <h1>CONTACT US</h1>
-                <p>...</p>
-            </div>
+        <div class="section-padding align-centre lgrey">
+            <h1>CONTACT US</h1>
+            <p>Please do not hesitate to contact us regarding any matter relating to this Privacy Policy at <a href="mailto:info@cell-industries.co.uk">info@cell-industries.co.uk</a></p>
         </div>
     </section>
 
