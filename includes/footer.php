0ca732dd1680696d57a5285e86784c1d1fe5ba57
jamesl1001 <jamesl1001@msn.com>
2014-03-24 18:38:03 +0000

    Add template Privacy Policy page and link in footer. Modify fractions.scss so half-margins work when there are more than two.

--- includes/footer.php
+++ includes/footer.php
@@ -1,9 +1,11 @@
 <footer class="section-padding align-centre dgrey">
     <p id="copyright">&copy; Copyright Cell Industries 2150</p>
+    <a href="/privacy-policy" id="privacy"><p>Privacy policy</p></a>
     <div id="social-icons">
         <a href="#" class="btn social-btn ico-facebook"></a>
         <a href="#" class="btn social-btn ico-twitter"></a>
     </div>
     <div class="cell-logo">
         <a href="/" class="ico-cell-logo"></a>
-    </div></footer>
+    </div>
+</footer>
---
+++ views/privacy-policy.php
@@ -0,0 +1,54 @@
+<header id="fixed-header" class="fixed-header--privacy section-padding">
+    <hgroup class="align-vertical">
+        <h1>PRIVACY POLICY</h1>
+        <?php require_once('includes/balls.php'); ?>
+    </hgroup>
+</header>
+
+<main>
+
+    <section>
+        <div class="section-padding align-centre lgrey">
+            <div class="section-content-wrapper half-margin">
+                <h1></i>INFORMATION WE COLLECT</h1>
+                <p>...</p>
+            </div>
+
+            <div class="section-content-wrapper half-margin">
+                <h1></i>USE OF COOKIES</h1>
+                <p>...</p>
+            </div>
+
+            <div class="section-content-wrapper half-margin">
+                <h1></i>USE OF YOUR INFORMATION</h1>
+                <p>...</p>
+            </div>
+
+            <div class="section-content-wrapper half-margin">
+                <h1></i>STORING YOUR PERSONAL DATA</h1>
+                <p>...</p>
+            </div>
+
+            <div class="section-content-wrapper half-margin">
+                <h1></i>DISCLOSING YOUR PERSONAL INFORMATION</h1>
+                <p>...</p>
+            </div>
+
+            <div class="section-content-wrapper half-margin">
+                <h1></i>THIRD PARTY LINKS</h1>
+                <p>...</p>
+            </div>
+
+            <div class="section-content-wrapper half-margin">
+                <h1></i>ACCESS TO INFORMATION</h1>
+                <p>...</p>
+            </div>
+
+            <div class="section-content-wrapper half-margin">
+                <h1></i>CONTACT US</h1>
+                <p>...</p>
+            </div>
+        </div>
+    </section>
+
+</main>
