b9b725203cfab61a18f460fd2b5d376ccc878b25
Jacob Hammond <ss_clan_cod@hotmail.com>
2014-03-25 23:40:58 +0000

    Added 404 page styles and markup

--- views/404.php
+++ views/404.php
@@ -1,4 +1,4 @@
-<header id="fixed-header" class="fixed-header--404 section-padding">
+<header id="fixed-header" class="section-padding">
     <hgroup class="align-vertical">
         <h1>404: PAGE NOT FOUND</h1>
     </hgroup>
@@ -6,9 +6,18 @@
 
 <main>
 
-        <section>
-        <div class="section-padding align-centre mblue">
-            <p><i class="ico-info"></i>Oops! Sorry, but the page you are looking for has not been found. Try checking the URL for errors or <a href="/">return to the homepage.</a></p>
+    <div class="cube-wrapper-2">
+        <div class="cube">
+            <div class="front"></div>
+            <div class="back"></div>
+            <div class="left"></div>
+            <div class="right"></div>
+            <img src="/img/sad.png" alt="404: Page not found">
+        </div>
+    </div>
+    <section id="fourohfour-text">
+        <div class="section-padding align-centre">
+            <p class="dgrey"><i class="ico-info"></i>Oops! Sorry, but the page you are looking for has not been found. Try checking the URL for errors or <a href="/">return to the homepage.</a></p>
         </div>
     </section>
 
