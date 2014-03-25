a9e51c9b754797a7194748978eba5bb99c5ecd05
Jacob Hammond <ss_clan_cod@hotmail.com>
2014-03-25 20:13:09 +0000

    Updated text - added slide 4/5 text

--- views/the-cloning-process.php
+++ views/the-cloning-process.php
@@ -4,24 +4,24 @@
     <h1 id="ig-title">The Cloning Process</h1>
         <div class="ig-slide">
             <div class="ig-slide ig-slide--1">
-            <p class="ig-text">The Quantum Clone Cube, powered by its nuclear fusion reactor power source, travels from our deep space storage facility through an Einstein Rosenbridge portal.</p>
+            <p class="ig-text">The Quantum Clone Cube, powered by its Nuclear Fusion Reactor Power Source, travels from our deep space storage facility through an Einstein Rosenbridge portal.</p>
             </div>
         </div>
         <div class="ig-slide">
             <div class="ig-slide ig-slide--2"></div>
             <img src="/img/infographic/satellite.png" alt="Satellite" id="ig-satellite" class="ig-animate"/>
-            <p class="ig-text">The Clone Cube enters the Earth's atmosphere, protected by its Thermo-resistant shell, coated with Teflon 3.0.</p>
+            <p class="ig-text">The Clone Cube enters the Earth's atmosphere, protected by its Thermo-Resistant Shell, coated with Teflon 3.0.</p>
         </div>
         <div class="ig-slide">
             <div class="ig-slide ig-slide--3"></div>
             <img src="/img/infographic/plane.png" alt="Plane" id="ig-plane" class="ig-animate"/>
-            <p class="ig-text">When approaching the Earths surface the Cube's navigational thrusters are activated, helping it slow down and allowing for more refined movements towards it's target location.</p>
+            <p class="ig-text">When approaching the Earths surface the Cube's Navigational Thrusters are activated, helping it slow down and allowing for more refined movements towards it's target location.</p>
         </div>
         <div class="ig-slide">
             <div class="ig-slide ig-slide--4"></div>
             <div id="ig-scanner-wrapper" class="ig-animate"><img src="/img/infographic/scanner.png" alt="Scanner" id="ig-scanner"/></div>
-            <p class="ig-text ig-text--4">Scanning.</p>
-            <p class="ig-text ig-text--5">The Quantum Clone Cube returns to our deep space storage facility for safe keeping.</p>
+            <p class="ig-text ig-text--4">Using its Topographic Profile Scanner the Cube generates a profile of the envronment it's scanning.</p>
+            <p class="ig-text ig-text--5">Once the environment scan is complete the Cube returns to our deep space storage facility for safe keeping.</p>
         </div>
     </div>
     <img src="/img/infographic/clone-cube.png" id="ig-cube" alt="Clone Cube"/>
