c660ee3b5623e71dc55802d980be1771d04326b8
jamesl1001 <jamesl1001@msn.com>
2014-03-26 16:50:55 +0000

    Add cookies legal message

--- front-view.php
+++ front-view.php
@@ -6,6 +6,7 @@
     <?php require_once('includes/head.php'); ?>
 </head>
 <body>
+    <?php require_once('includes/cookies.php'); ?>
     <?php require_once('includes/header.php'); ?>
     <?php require_once("views/$file.php"); ?>
     <?php require_once('includes/footer.php'); ?>
