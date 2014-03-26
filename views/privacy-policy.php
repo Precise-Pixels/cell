3a61c1a71d2f2ee692d914a7e088f45132944641
Jacob Hammond <ss_clan_cod@hotmail.com>
2014-03-26 19:21:05 +0000

    Made every other section mblue

--- views/privacy-policy.php
+++ views/privacy-policy.php
@@ -18,7 +18,7 @@
             </ul>
         </div>
 
-        <div class="section-padding align-centre lgrey">
+        <div class="section-padding align-centre mblue">
             <h1>USE OF COOKIES</h1>
             <p>When using our site, we may set cookies on your computer in order to assist with providing you a great user experience.</p>
             <p>We or our third-party providers may set the following cookies whilst using our site:</p>
@@ -42,7 +42,7 @@
             </ul>
         </div>
 
-        <div class="section-padding align-centre lgrey">
+        <div class="section-padding align-centre mblue">
             <h1>STORING YOUR PERSONAL DATA</h1>
             <p>All passwords are securely encrypted.</p>
             <p>In operating our website it is necessary to transfer data that we collect from you to locations outside of the European Union for processing and storing. By providing your personal data to us, you agree to this transfer, storing or processing. We do our utmost to ensure that all reasonable steps are taken to make sure that your data is stored securely.</p>
@@ -54,7 +54,7 @@
             <p>We will not disclose your personal information to any other party without your prior permission other than in accordance with this Privacy Policy and where we are legally required by law.</p>
         </div>
 
-        <div class="section-padding align-centre lgrey">
+        <div class="section-padding align-centre mblue">
             <h1>THIRD PARTY LINKS</h1>
             <p>On occasion we include links to third parties on this website. Where we provide a link it does not mean that we endorse or approve that site’s policy towards visitor privacy. You should review their privacy policy before sending them any personal data.</p>
         </div>
@@ -64,7 +64,7 @@
             <p>In accordance with the Data Protection Act 1998 you have the right to access any information that we hold relating to you. Please note that we reserve the right to charge a fee of £10 to cover costs incurred by us in providing you with the information.</p>
         </div>
 
-        <div class="section-padding align-centre lgrey">
+        <div class="section-padding align-centre mblue">
             <h1>CONTACT US</h1>
             <p>Please do not hesitate to contact us regarding any matter relating to this Privacy Policy at <a href="mailto:info@cell-industries.co.uk">info@cell-industries.co.uk</a></p>
         </div>
