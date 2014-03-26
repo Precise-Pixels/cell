6c64709c3b480ddc9471a1261598b5523eb8e834
Jacob Hammond <ss_clan_cod@hotmail.com>
2014-03-26 18:56:55 +0000

    Removed logo diagram section and relevant styles

--- views/about.php
+++ views/about.php
@@ -7,12 +7,6 @@
 <main>
 
     <section>
-        <div id="about-logo-header" class="align-centre mblue">
-            <i class="ico-cell-logo"></i>
-        </div>
-    </section>
-
-    <section>
         <div class="section-padding align-centre lgrey">
             <h1><i class="ico-company"></i>THE COMPANY</h1>
             <p class="half-margin">As the fastest growing company on Earth, Cell Industries is always striving for a better world through technology. We believe the key to constant progression is investment in the people who also believe in technology as a means for a better future. We invest heavily in our people to help make their visions a reality.</p>
