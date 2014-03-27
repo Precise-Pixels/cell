e0db64947e6ceca30184d992e2b3afe9d8f1e71d
Jacob Hammond <ss_clan_cod@hotmail.com>
2014-03-27 18:13:28 +0000

    Updated company text

--- views/about.php
+++ views/about.php
@@ -9,8 +9,8 @@
     <section>
         <div class="section-padding align-centre lgrey">
             <h1><i class="ico-company"></i>THE COMPANY</h1>
-            <p class="half-margin">As the fastest growing company on Earth, Cell Industries is always striving for a better world through technology. We believe the key to constant progression is investment in the people who also believe in technology as a means for a better future. We invest heavily in our people to help make their visions a reality.</p>
-            <p class="half-margin">Every day Cell Industries pushes itself further and is constantly devoted to discovering new and groundbreaking technologies. The kind of technologies that can help everyone move into a happier and more prosperous future together.</p>
+            <p class="half-margin">As the fastest growing company on Earth, Cell Industries is always striving for a better world through technology. We believe the key to constant progression is investment in the people who also believe in technology as a means for a better future.</p>
+            <p class="half-margin">Every day Cell Industries pushes itself further and is constantly devoted to discovering new and groundbreaking technologies that will help everyone move into a happier and more prosperous future together.</p>
         </div>
     </section>
 
