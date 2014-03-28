9e9732e985ff86f528985d6459f18db1b15d5070
jamesl1001 <jamesl1001@msn.com>
2014-03-28 22:30:18 +0000

    Change cookies-message cookie to camelCase to match other cookies

--- includes/cookies.php
+++ includes/cookies.php
@@ -1,6 +1,6 @@
 <?php
-if(!isset($_COOKIE['cookies-message'])):
-    setcookie('cookies-message', 'true', time()+(60*60*24*365), '/');
+if(!isset($_COOKIE['cookiesMessage'])):
+    setcookie('cookiesMessage', 'true', time()+(60*60*24*365), '/');
 ?>
     <div id="cookies" class="dgrey"><p><i class="ico-cross"></i>This site uses cookies. Some of the cookies we use are essential for parts of the site to operate and have already been set. You may delete and block all cookies from this site, but parts of the site will not work.</p><p><a href="/privacy-policy">Click here to learn more.</a></p></div>
     <script>var cookies = document.getElementById('cookies'); cookies.addEventListener('click', function() { cookies.className = 'cookies--hide'; });</script>
