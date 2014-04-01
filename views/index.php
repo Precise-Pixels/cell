94c51495fe2724b8250a7064e7c3279a2c15d813
Jacob Hammond <ss_clan_cod@hotmail.com>
2014-04-01 22:22:26 +0100

    Restructured homepage markup

--- views/index.php
+++ views/index.php
@@ -10,19 +10,43 @@
 
 <main id="homepage-main">
 
-    <section>
+<!--     <section id="homepage-three">
         <div class="align-centre section-padding lgrey">
-            <div class="half-margin">
+            <div class="third-margin">
+                <h2></i>DECIDE</h2>
+                <p>You get to choose which parts of the planet you would like to preserve.</p>
+            </div>
+            <div class="third-margin">
+                <h2></i>CLONE</h2>
+                <p>Your chosen environments are cloned using our groundbreaking QuantumCell™ technology.</p>
+            </div>
+            <div class="third-margin">
+                <h2></i>PRESERVE</h2>
+                <p>Environments are stored in deep space, protected and ready to be restored in the event of undesirable occurrences.</p>
+            </div>
+        </div>
+    </section> -->
+    <section id="homepage-top">
+        <div class="align-centre lgrey">
+            <div class="section-padding half">
                 <h1><i class="ico-project-titan"></i>WHAT IS PROJECT TITAN?</h1>
                 <p>In an ever changing and increasingly turbulent world the natural environments of our planet often bear the brunt of modern society. Using our groundbreaking QuantumCell&trade; technology we want to ensure the environments that matter to you are preserved for millennia to come. We can clone whole areas of environments leaving them protected and ready to be restored in the event of undesirable occurrences.</p>
             </div>
-            <div class="half-margin">
+            <div class="section-padding half">
+                <img src="img/project-titan-earth.jpg" alt="">
+            </div>
+        </div>
+        <section>
+        <div class="align-centre lgrey">
+            <div class="section-padding half">
+                <img src="img/project-titan-scanning.jpg" alt="">
+            </div>
+            <div class="section-padding half">
                 <h1><i class="ico-my-cell"></i>HOW ARE YOU INVOLVED?</h1>
                 <p>Project Titan is a collaborative project designed to involve everyone in preserving the Earth's environments. You get to decide which part of the planet we preserve next. You will be able to view the 3D representation of the environments you clone at any time, along with live information associated with those environments. You can then share your environments and let everyone know that you have done your part to help preserve the planet.</p>
             </div>
         </div>
     </section>
-
     <section id="homepage-diagram">
         <div class="section-padding align-centre mblue">
             <h1>Simply register for free, follow the instructions and build up your own collection of cloned environments.</h2>
