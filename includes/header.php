b89fd6491dfa99f7c19838472fa638d65362cecf
faooful <faooful@gmail.com>
2014-04-01 02:53:35 +0100

    cleaned up the sass of unneeded styles.

--- includes/header.php
+++ includes/header.php
@@ -15,9 +15,9 @@
 
     <?php if(isset($_SESSION['status']) && $_SESSION['status'] == 'signedin'): ?>
         <a href="/user/<?= $_SESSION['username']; ?>" title="Your Profile "><i class="user-nav-profile ico-my-cell"></i></a>
-        <a href="/signout?r=<?= $_SERVER['REQUEST_URI']; ?>" title="Sign Out"><p class="user-message">Logout</p></i></a>
+        <a href="/signout?r=<?= $_SERVER['REQUEST_URI']; ?>" title="Sign Out"><p class="login-status">Logout</p></i></a>
     <?php else: ?>
-        <a href="/signin" title="Sign In"><p class="user-message">Sign in / Register</p></i></a>
+        <a href="/signin" title="Sign In"><p class="login-status">Sign in / Register</p></i></a>
     <?php endif; ?>
 
     <div class="cell-logo">
