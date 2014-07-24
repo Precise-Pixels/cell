035a7238ab9b934774b5852b9a48ba1095d57d14
jamesl1001 <jamesl1001@msn.com>
2014-07-24 13:39:36 +0100

    Fix cURL error

--- model-fyp.php
+++ model-fyp.php
@@ -1,21 +1,23 @@
 <?php
 
-$url  = 'https://api.github.com/repos/Precise-Pixels/cell/stats/contributors';
+$url  = 'https://api.github.com/repos/Precise-Pixels/cell/contributors';
 $curl = curl_init();
 curl_setopt_array($curl, array(
     CURLOPT_RETURNTRANSFER => 1,
     CURLOPT_URL => $url,
-    CURLOPT_USERAGENT => 'JaL-User-Agent'));
+    CURLOPT_USERAGENT => 'JaL-User-Agent',
+    CURLOPT_SSL_VERIFYPEER => false));
 $contributors = json_decode(curl_exec($curl));
 
-$commits = $contributors[0]->total + $contributors[1]->total + $contributors[2]->total;
+$commits = $contributors[0]->contributions + $contributors[1]->contributions + $contributors[2]->contributions;
 
 $url  = 'https://api.github.com/repos/Precise-Pixels/cell/branches?per_page=999';
 $curl = curl_init();
 curl_setopt_array($curl, array(
     CURLOPT_RETURNTRANSFER => 1,
     CURLOPT_URL => $url,
-    CURLOPT_USERAGENT => 'JaL-User-Agent'));
+    CURLOPT_USERAGENT => 'JaL-User-Agent',
+    CURLOPT_SSL_VERIFYPEER => false));
 $branches = json_decode(curl_exec($curl));
 
 $url  = 'https://api.github.com/repos/Precise-Pixels/cell/pulls?state=all&per_page=999';
@@ -23,5 +25,6 @@ $curl = curl_init();
 curl_setopt_array($curl, array(
     CURLOPT_RETURNTRANSFER => 1,
     CURLOPT_URL => $url,
-    CURLOPT_USERAGENT => 'JaL-User-Agent'));
+    CURLOPT_USERAGENT => 'JaL-User-Agent',
+    CURLOPT_SSL_VERIFYPEER => false));
 $pulls = json_decode(curl_exec($curl));
